bfb74dd1e831c094e26905e6b7e4ec49f92aa2de
dimanu-py <diegomtz126@gmail.com>
2025-02-26 10:35:49 +0100

    ♻️ refactor(project-generator): create project structure inside a temporary directory

--- instant_python/project_generator/folder_tree.py
+++ instant_python/project_generator/folder_tree.py
@@ -1,3 +1,4 @@
+import tempfile
 from pathlib import Path
 
 from instant_python.project_generator.node import Node, NodeType
@@ -8,8 +9,9 @@ from instant_python.project_generator.python_module import PythonModule
 class FolderTree:
     def create(self, project_structure: dict) -> None:
         tree = [self._build_tree(node) for node in project_structure.get("root", [])]
+        temporary_directory = Path(tempfile.mkdtemp())
         for node in tree:
-            node.create(base_path=Path.cwd())
+            node.create(base_path=temporary_directory)
 
     def _build_tree(self, node: dict) -> Node:
         node_type = node.get("type")
