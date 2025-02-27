97fc1045a9f444d2425fdc9c1a1e5a62485ad8f4
dimanu-py <diegomtz126@gmail.com>
2025-02-27 14:59:07 +0100

    ♻️ refactor(project-generator): create a single node type File that will work with any kind of file

--- instant_python/project_generator/file.py
+++ instant_python/project_generator/file.py
@@ -4,18 +4,18 @@ from instant_python.project_generator.node import Node
 from instant_python.project_generator.template_manager import TemplateManager
 
 
-class PythonFile(Node):
-    _EXTENSION = ".py"
+class File(Node):
 
-    def __init__(self, name: str) -> None:
+    def __init__(self, name: str, extension: str) -> None:
         self._name = name
+        self._extension = extension
         self._template_manager = TemplateManager()
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}(name={self._name})"
 
     def create(self, base_path: Path) -> None:
-        file_name = f"{self._name}{self._EXTENSION}"
+        file_name = f"{self._name}{self._extension}"
         file_path = base_path / file_name
 
         template_path = Path("boilerplate") / file_name
