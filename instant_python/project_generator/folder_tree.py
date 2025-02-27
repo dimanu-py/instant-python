cf30a5f8a75801f2493594b23e7d56f2bc1e54ff
dimanu-py <diegomtz126@gmail.com>
2025-02-27 14:59:47 +0100

    ♻️ refactor(project-generator): add extension field to node and remove deprecated options

--- instant_python/project_generator/folder_tree.py
+++ instant_python/project_generator/folder_tree.py
@@ -3,9 +3,8 @@ from pathlib import Path
 
 from instant_python.project_generator.directory import Directory
 from instant_python.project_generator.node import Node, NodeType
-from instant_python.project_generator.python_file import PythonFile
+from instant_python.project_generator.file import File
 from instant_python.project_generator.python_module import PythonModule
-from instant_python.project_generator.yml_file import YmlFile
 
 
 class FolderTree:
@@ -23,12 +22,11 @@ class FolderTree:
         if node_type == NodeType.PYTHON_MODULE:
             module_children = [self._build_tree(child) for child in children]
             return PythonModule(name=name, children=module_children)
-        elif node_type == NodeType.PYTHON_FILE:
-            return PythonFile(name=name)
         elif node_type == NodeType.DIRECTORY:
             directory_children = [self._build_tree(child) for child in children]
             return Directory(name=name, children=directory_children)
-        elif node_type == NodeType.YML_FILE:
-            return YmlFile(name=name)
+        elif node_type == NodeType.FILE:
+            extension = node.get("extension", "")
+            return File(name=name, extension=extension)
         else:
             raise ValueError(f"Invalid node type: {node_type}")
