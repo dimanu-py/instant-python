38f7baa7e1befc3527f81ded79102315e33e4fd9
dimanu-py <diegomtz126@gmail.com>
2025-03-01 17:27:33 +0100

    ♻️ refactor(project-generator): modify File class to be able to manage the difference between the path to the template and the path where the file should be written

--- instant_python/project_generator/file.py
+++ instant_python/project_generator/file.py
@@ -7,17 +7,14 @@ from instant_python.project_generator.template_manager import TemplateManager
 class File(Node):
 
     def __init__(self, name: str, extension: str) -> None:
-        self._name = name
-        self._extension = extension
+        self._file_name = f"{name.split("/")[-1]}{extension}"
+        self._template_path = f"boilerplate/{name}{extension}"
         self._template_manager = TemplateManager()
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__name__}(name={self._name})"
+        return f"{self.__class__.__name__}(name={self._file_name})"
 
     def create(self, base_path: Path) -> None:
-        file_name = f"{self._name}{self._extension}"
-        file_path = base_path / file_name
-
-        template_path = Path("boilerplate") / file_name
-        content = self._template_manager.get_boilerplate(str(template_path))
+        file_path = base_path / self._file_name
+        content = self._template_manager.get_boilerplate(self._template_path)
         file_path.write_text(content)
