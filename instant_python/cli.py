a413865f718ce58952ac13ae9636f2e803d6702b
dimanu-py <diegomtz126@gmail.com>
2025-03-01 22:46:37 +0100

    ♻️ refactor(cli): generate user requirements only if no other file has been already generated.
    The ide behind this is to be able to run the questions just once and the be able to re-run the application without needing to answer the questions again

--- instant_python/cli.py
+++ instant_python/cli.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import typer
 
 from instant_python.project_generator.project_generator import ProjectGenerator
@@ -6,10 +8,15 @@ from instant_python.question_prompter.basic_prompter import BasicPrompter
 app = typer.Typer()
 
 
+def user_requirements_has_not_been_generated_before() -> bool:
+    return Path("user_requirements.yml").exists() is False
+
+
 @app.command()
 def generate_project():
-    user_requirements = BasicPrompter().ask()
-    user_requirements.save_in_memory()
+    if user_requirements_has_not_been_generated_before():
+        user_requirements = BasicPrompter().ask()
+        user_requirements.save_in_memory()
 
     project_generator = ProjectGenerator()
     project_generator.generate()
