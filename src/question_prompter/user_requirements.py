2f5fe200f90ecaac2a4a50d86cde3ac51f2a41ed
dimanu-py <diegomtz126@gmail.com>
2025-03-03 07:44:29 +0100

    ✨ feat(prompter): add load_from_file method to UserRequirements

--- src/question_prompter/user_requirements.py
+++ src/question_prompter/user_requirements.py
@@ -1,33 +1,39 @@
 from dataclasses import dataclass, asdict, field
 from datetime import datetime
+from typing import Self
 
 import yaml
 
 
 @dataclass
 class UserRequirements:
-	project_name: str
-	project_slug: str
-	source_name: str
-	license: str
-	version: str
-	description: str
-	author: str
-	email: str
-	python_version: str
-	dependency_manager: str
-	python_manager: str
-	default_dependencies: bool
-	template: str
-	bounded_context: str = field(default=None)
-	aggregate_name: str = field(default=None)
-	git: bool = field(default=False)
-	built_in_features: list[str] = field(default_factory=list)
-	year: int = field(default=datetime.now().year)
+    project_name: str
+    project_slug: str
+    source_name: str
+    license: str
+    version: str
+    description: str
+    author: str
+    email: str
+    python_version: str
+    dependency_manager: str
+    python_manager: str
+    default_dependencies: bool
+    template: str
+    bounded_context: str = field(default=None)
+    aggregate_name: str = field(default=None)
+    git: bool = field(default=False)
+    built_in_features: list[str] = field(default_factory=list)
+    year: int = field(default=datetime.now().year)
 
-	def to_dict(self) -> dict:
-		return asdict(self)
+    def to_dict(self) -> dict:
+        return asdict(self)
 
-	def save_in_memory(self):
-		with open("user_requirements.yml", "w") as file:
-			yaml.dump(self.to_dict(), file)
+    def save_in_memory(self) -> None:
+        with open("user_requirements.yml", "w") as file:
+            yaml.dump(self.to_dict(), file)
+
+    @classmethod
+    def load_from_file(cls) -> Self:
+        with open("user_requirements.yml") as file:
+            return cls(**yaml.safe_load(file))
