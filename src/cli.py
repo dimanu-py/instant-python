f3fbd417dd662d339ec4df91b18c0b82a4347a58
dimanu-py <diegomtz126@gmail.com>
2025-03-03 07:44:50 +0100

    ✨ feat(cli): make sure user_requirements are loaded

--- src/cli.py
+++ src/cli.py
@@ -4,6 +4,7 @@ import typer
 
 from src.project_generator.project_generator import ProjectGenerator
 from src.question_prompter.basic_prompter import BasicPrompter
+from src.question_prompter.user_requirements import UserRequirements
 
 app = typer.Typer()
 
@@ -17,6 +18,8 @@ def generate_project():
     if user_requirements_has_not_been_generated_before():
         user_requirements = BasicPrompter().ask()
         user_requirements.save_in_memory()
+    else:
+        user_requirements = UserRequirements.load_from_file()
 
     project_generator = ProjectGenerator()
     project_generator.generate()
