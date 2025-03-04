96c2fc8c14690db5a858bfd8c23a868173277a2a
dimanu-py <diegomtz126@gmail.com>
2025-03-04 07:28:07 +0100

    ✨ feat(prompter): create BooleanQuestion for yes or no questions

--- src/question_prompter/question.py
+++ src/question_prompter/question.py
@@ -7,7 +7,6 @@ class Question[T](ABC):
     key: str
     message: str
     default: str = field(default_factory=str)
-    confirm: bool = field(default=False)
     multiselect: bool = field(default=False)
 
     @abstractmethod
