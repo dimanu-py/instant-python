da4bec281f5ce72f7e948ab4ccbb9c522ac3467a
dimanu-py <diegomtz126@gmail.com>
2025-03-04 07:22:41 +0100

    ✨ feat(prompter): create ChoiceQuestion to encapsulate questions that have different options the user needs to choose from

--- src/question_prompter/question.py
+++ src/question_prompter/question.py
@@ -4,13 +4,12 @@ from dataclasses import dataclass, field
 
 @dataclass(frozen=True)
 class Question[T](ABC):
-	key: str
-	message: str
-	default: str = field(default_factory=str)
-	options: list[str] = field(default_factory=list)
-	confirm: bool = field(default=False)
-	multiselect: bool = field(default=False)
+    key: str
+    message: str
+    default: str = field(default_factory=str)
+    confirm: bool = field(default=False)
+    multiselect: bool = field(default=False)
 
-	@abstractmethod
-	def ask(self) -> T:
-	    raise NotImplementedError
+    @abstractmethod
+    def ask(self) -> T:
+        raise NotImplementedError
