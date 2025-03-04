c8d7c37c23ac4aa77eb79bb0bad8d5c90bb8acaa
dimanu-py <diegomtz126@gmail.com>
2025-03-04 07:22:02 +0100

    ♻️ refactor(prompter): make Question abstract and add ask abstract method

--- src/question_prompter/question.py
+++ src/question_prompter/question.py
@@ -1,11 +1,16 @@
+from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
 
 
 @dataclass(frozen=True)
-class Question:
+class Question[T](ABC):
 	key: str
 	message: str
 	default: str = field(default_factory=str)
 	options: list[str] = field(default_factory=list)
 	confirm: bool = field(default=False)
 	multiselect: bool = field(default=False)
+
+	@abstractmethod
+	def ask(self) -> T:
+	    raise NotImplementedError
