e6da54651f9c86825db1001530aa7571c706502c
dimanu-py <diegomtz126@gmail.com>
2025-03-04 07:41:32 +0100

    ♻️ refactor(prompter): delegate ask logic to each question instead of letting prompter what to do depending on flags

--- src/question_prompter/basic_prompter.py
+++ src/question_prompter/basic_prompter.py
@@ -10,46 +10,18 @@ from src.question_prompter.user_requirements import UserRequirements
 
 
 class BasicPrompter:
-    def ask(self) -> UserRequirements:
+    @staticmethod
+    def ask() -> UserRequirements:
         answers = {
-            question.key: self._ask_single_question(question)
+            question.key: question.ask()
             for question in GENERAL_QUESTIONS
         }
 
         if answers["template"] == TemplateTypes.DDD:
             ddd_answers = {
-                question.key: self._ask_single_question(question)
+                question.key: question.ask()
                 for question in DDD_QUESTIONS
             }
             answers.update(ddd_answers)
 
         return UserRequirements(**answers)
-
-    def _ask_single_question(self, question: Question) -> str | bool | list[str]:
-        if question.confirm:
-            return self._confirm(question.message)
-        elif question.multiselect:
-            return self._multiselect(question.message, question.options)
-        return self._prompt(question.message, question.default, question.options)
-
-    @staticmethod
-    def _prompt(
-        text: str,
-        default_value: str,
-        options: list[str],
-    ) -> str:
-        if not options:
-            return questionary.text(text, default=default_value).ask()
-        return questionary.select(
-            text,
-            choices=options,
-            default=default_value,
-        ).ask()
-
-    @staticmethod
-    def _confirm(text: str) -> bool:
-        return questionary.confirm(text, default=True).ask()
-
-    @staticmethod
-    def _multiselect(message: str, options: list[str]) -> list[str]:
-        return questionary.checkbox(message, choices=options).ask()
