094498466247b2a476a44a9cd69b12db9e822ead
dimanu-py <diegomtz126@gmail.com>
2025-03-08 17:23:26 +0100

    ✨ feat(prompter): implement Steps collection and Step interface

---
+++ src/question_prompter/steps.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+from collections.abc import Iterator
+
+
+class Step(ABC):
+    @abstractmethod
+    def run(self, answers_so_far: dict[str, str]) -> dict[str, str]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def should_not_ask(self, answers_so_far: dict[str, str]) -> bool:
+        raise NotImplementedError
+
+
+class Steps:
+    def __init__(self, *step: Step) -> None:
+        self._steps = list(step)
+
+    def __iter__(self) -> Iterator[Step]:
+        return iter(self._steps)
