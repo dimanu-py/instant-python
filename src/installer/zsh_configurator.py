2557a0d2cc7618026907bca80f5c5fd879fceaac
dimanu-py <diegomtz126@gmail.com>
2025-03-09 20:34:22 +0100

    🎨 style(installer): reformat ZshConfigurator

--- src/installer/zsh_configurator.py
+++ src/installer/zsh_configurator.py
@@ -1,4 +1,5 @@
 import subprocess
+from typing import override
 
 from src.installer.shell_configurator import ShellConfigurator
 
@@ -8,7 +9,10 @@ class ZshConfigurator(ShellConfigurator):
         self._executable = "/bin/bash"
         self._command = r"export PATH=\"$HOME/.local/bin:$PATH\""
 
+    @override
     def configure_shell_completion(self) -> None:
         print(">>> Configuring zsh...")
-        subprocess.run(self._command, shell=True, check=True, executable=self._executable)
+        subprocess.run(
+            self._command, shell=True, check=True, executable=self._executable
+        )
         print(">>> zsh configured successfully")
