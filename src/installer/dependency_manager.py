7621ac17fc04898c08e5f79315094d4e5986d809
dimanu-py <diegomtz126@gmail.com>
2025-03-09 12:37:08 +0100

    ✨ feat(installer): add install_dependencies method to DependencyManger interface

--- src/installer/dependency_manager.py
+++ src/installer/dependency_manager.py
@@ -9,3 +9,7 @@ class DependencyManager(ABC):
     @abstractmethod
     def install_python(self, version: str) -> None:
         raise NotImplementedError
+
+    @abstractmethod
+    def install_dependencies(self, dependencies: list[str]) -> None:
+        raise NotImplementedError
