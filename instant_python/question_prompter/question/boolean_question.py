e085842a1e82e1dfc5db6821ff5c3d4dcb12f782
dimanu-py <diegomtz126@gmail.com>
2025-03-13 23:48:04 +0100

    💥 refactor(instant-python)!: rename src folder to instant_python to comply package name configuration

---
+++ instant_python/question_prompter/question/boolean_question.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass, field
+
+import questionary
+
+from instant_python.question_prompter.question.question import Question
+
+
+@dataclass(frozen=True)
+class BooleanQuestion(Question[bool]):
+    default: bool = field(default=True)
+
+    def ask(self) -> bool:
+        return questionary.confirm(self.message, default=self.default).ask()
