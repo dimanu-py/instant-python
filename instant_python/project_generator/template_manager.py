d3854accc3f9a59b1daf4d6f87b3c73c1746c955
dimanu-py <diegomtz126@gmail.com>
2025-03-20 21:08:37 +0100

    🎨 style(project-generator): reformat template_manager.py

--- instant_python/project_generator/template_manager.py
+++ instant_python/project_generator/template_manager.py
@@ -2,8 +2,6 @@ from abc import ABC, abstractmethod
 
 
 class TemplateManager(ABC):
-
     @abstractmethod
     def get_project(self, template_name: str) -> dict:
         raise NotImplementedError
-    
