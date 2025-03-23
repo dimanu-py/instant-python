c9535b5ff5466810171d8745cebbe5eb5f55dd41
dimanu-py <diegomtz126@gmail.com>
2025-03-23 16:36:46 +0100

    ♻ refactor(prompter): substitute set of question in GitStep for ConditionalQuestion

--- instant_python/question_prompter/step/git_step.py
+++ instant_python/question_prompter/step/git_step.py
@@ -1,4 +1,5 @@
 from instant_python.question_prompter.question.boolean_question import BooleanQuestion
+from instant_python.question_prompter.question.conditional_question import ConditionalQuestion
 from instant_python.question_prompter.question.free_text_question import FreeTextQuestion
 from instant_python.question_prompter.step.steps import Step
 
@@ -6,17 +7,17 @@ from instant_python.question_prompter.step.steps import Step
 class GitStep(Step):
     def __init__(self) -> None:
         self._questions = [
-            BooleanQuestion(key="git", message="Do you want to initialize a git repository?", default=True),
-            FreeTextQuestion(key="git_user_name", message="Type your git user name"),
-            FreeTextQuestion(key="git_email", message="Type your git email"),
+            ConditionalQuestion(
+                base_question=BooleanQuestion(key="git", message="Do you want to initialize a git repository?", default=True),
+                subquestions=[
+                    FreeTextQuestion(key="git_user_name", message="Type your git user name"),
+                    FreeTextQuestion(key="git_email", message="Type your git email"),
+                ],
+                condition=True
+            )
         ]
 
     def run(self, answers_so_far: dict[str, str]) -> dict[str, str]:
-        initialize_git_repo = self._questions[0].ask()
-        answers_so_far[self._questions[0].key] = initialize_git_repo
-        if not initialize_git_repo:
-            return answers_so_far
-
-        for question in self._questions[1:]:
-            answers_so_far[question.key] = question.ask()
+        for question in self._questions:
+            answers_so_far.update(question.ask())
         return answers_so_far
