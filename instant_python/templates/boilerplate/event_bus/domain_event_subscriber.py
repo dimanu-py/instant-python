1cc221178b8d92cd281cf50a700ff420270e3761
dimanu-py <diegomtz126@gmail.com>
2025-04-08 21:54:37 +0200

    ♻ refactor(template): modify DomainEventSubscriber boilerplate to follow generic type syntax depending on python version

--- instant_python/templates/boilerplate/event_bus/domain_event_subscriber.py
+++ instant_python/templates/boilerplate/event_bus/domain_event_subscriber.py
@@ -1,4 +1,5 @@
 {% set template_domain_import = "shared.domain"|compute_base_path(template) %}
+{% if pythono_version in ["3.12", "3.13"] %}
 from abc import ABC, abstractmethod
 
 from {{ source_name }}.{{ template_domain_import }}.event.domain_event import DomainEvent
@@ -13,3 +14,21 @@ class DomainEventSubscriber[EventType: DomainEvent](ABC):
     @abstractmethod
     def on(self, event: EventType) -> None:
         raise NotImplementedError
+{% else %}
+from abc import ABC, abstractmethod
+from typing import Generic, TypeVar
+
+from {{ source_name }}.{{ template_domain_import }}.event.domain_event import DomainEvent
+
+EventType = TypeVar("EventType", bound=DomainEvent)
+
+class DomainEventSubscriber(Generic[EventType], ABC):
+    @staticmethod
+    @abstractmethod
+    def subscribed_to() -> list[type[EventType]]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def on(self, event: EventType) -> None:
+        raise NotImplementedError
+{% endif %}
