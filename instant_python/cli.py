d9721e57ef33d30287f1431830e472d67e43a995
dimanu-py <diegomtz126@gmail.com>
2025-04-15 18:33:56 +0200

    ✨ feat(cli): create main application based on custom implementation and add error handlers

--- instant_python/cli.py
+++ instant_python/cli.py
@@ -1,10 +1,24 @@
 import typer
-
 from instant_python import folder_cli, project_cli
+from instant_python.errors.application_error import ApplicationError
+from instant_python.intant_python_typer import InstantPythonTyper
 
-app = typer.Typer()
+app = InstantPythonTyper()
+
 app.add_typer(folder_cli.app, name="folder", help="Generate only the folder structure for a new project")
 app.add_typer(project_cli.app, name="project", help="Generate a full project ready to be used")
+
+
+@app.error_handler(ApplicationError)
+def handle_application_error(exc: ApplicationError) -> None:
+    typer.secho(f"Error: {exc.message}", err=True, fg=typer.colors.RED)
+    typer.Exit(code=-1)
+
+
+@app.error_handler(Exception)
+def handle_unexpected_error(exc: Exception) -> None:
+    typer.secho(f"An unexpected error occurred: {exc}", err=True, fg=typer.colors.RED)
+    typer.Exit(code=-1)
 
 
 if __name__ == "__main__":
