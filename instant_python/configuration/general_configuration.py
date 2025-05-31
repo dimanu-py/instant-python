319c5c944143b15e7a1ef8dc69796d10dde1e866
dimanu-py <diegomtz126@gmail.com>
2025-06-01 01:17:02 +0200

    ✨ feat(configuration): validate supported dependency managers in GeneralConfiguration

--- instant_python/configuration/general_configuration.py
+++ instant_python/configuration/general_configuration.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass
 
+from instant_python.configuration.invalid_dependency_manager_value import InvalidDependencyManagerValue
 from instant_python.configuration.invalid_license_value import InvalidLicenseValue
 from instant_python.configuration.invalid_version_value import InvalidPythonVersionValue
 
+SUPPORTED_DEPENDENCY_MANAGERS = ["uv", "pdm"]
 SUPPORTED_PYTHON_VERSIONS = ["3.10", "3.11", "3.12", "3.13"]
 SUPPORTED_LICENSES = ["MIT", "Apache", "GPL"]
 
@@ -21,6 +23,7 @@ class GeneralConfiguration:
     def __post_init__(self) -> None:
         self._ensure_license_is_supported()
         self._ensure_python_version_is_supported()
+        self._ensure_dependency_manager_is_supported()
 
     def _ensure_license_is_supported(self) -> None:
         if self.license not in SUPPORTED_LICENSES:
@@ -29,3 +32,7 @@ class GeneralConfiguration:
     def _ensure_python_version_is_supported(self) -> None:
         if self.python_version not in SUPPORTED_PYTHON_VERSIONS:
             raise InvalidPythonVersionValue(self.python_version)
+
+    def _ensure_dependency_manager_is_supported(self) -> None:
+        if self.dependency_manager not in SUPPORTED_DEPENDENCY_MANAGERS:
+            raise InvalidDependencyManagerValue(self.dependency_manager)
