9489ce7fdbe84ade80c0b00a87cc37e2dd2ffec3
dimanu-py <diegomtz126@gmail.com>
2025-06-01 01:46:51 +0200

    ♻️ refactor(configuration): join unsupported values test in a parametrized test

--- test/configuration/test_general_configuration.py
+++ test/configuration/test_general_configuration.py
@@ -1,3 +1,4 @@
+import pytest
 from expects import expect, raise_error
 
 from instant_python.configuration.invalid_dependency_manager_value import (
@@ -9,28 +10,32 @@ from test.configuration.general_configuration_mother import GeneralConfiguration
 
 
 class TestGeneralConfiguration:
-    def test_should_not_allow_to_create_general_configuration_with_invalid_license(
-        self,
-    ) -> None:
-        expect(lambda: GeneralConfigurationMother.with_parameter(license="BSD")).to(
-            raise_error(InvalidLicenseValue)
-        )
-
-    def test_should_not_allow_to_create_general_configuration_with_invalid_python_version(
-        self,
-    ) -> None:
-        expect(
-            lambda: GeneralConfigurationMother.with_parameter(python_version="3.9")
-        ).to(raise_error(InvalidPythonVersionValue))
-
-    def test_should_not_allow_to_create_general_configuration_with_invalid_dependency_manager(
-        self,
-    ) -> None:
-        expect(
-            lambda: GeneralConfigurationMother.with_parameter(dependency_manager="pip")
-        ).to(raise_error(InvalidDependencyManagerValue))
-
     def test_should_allow_to_create_general_configuration_with_valid_parameters(
         self,
     ) -> None:
         GeneralConfigurationMother.any()
+
+    @pytest.mark.parametrize(
+        "field, value, expected_error",
+        [
+            pytest.param("license", "BSD", InvalidLicenseValue, id="invalid_license"),
+            pytest.param(
+                "python_version",
+                "3.9",
+                InvalidPythonVersionValue,
+                id="invalid_python_version",
+            ),
+            pytest.param(
+                "dependency_manager",
+                "pip",
+                InvalidDependencyManagerValue,
+                id="invalid_dependency_manager",
+            ),
+        ],
+    )
+    def test_should_raise_error_for_unsupported_configuration_parameters(
+        self, field, value, expected_error
+    ) -> None:
+        expect(lambda: GeneralConfigurationMother.with_parameter(**{field: value})).to(
+            raise_error(expected_error)
+        )
