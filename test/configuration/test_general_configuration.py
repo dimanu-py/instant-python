1c985c9e23c370e318c3099961647a3ff14009e1
dimanu-py <diegomtz126@gmail.com>
2025-06-01 01:41:04 +0200

    🧪 test(configuration): add test to validate error is raised when trying to pass unsupported python version

--- test/configuration/test_general_configuration.py
+++ test/configuration/test_general_configuration.py
@@ -1,6 +1,7 @@
 from expects import expect, raise_error
 
 from instant_python.configuration.invalid_license_value import InvalidLicenseValue
+from instant_python.configuration.invalid_version_value import InvalidPythonVersionValue
 from test.configuration.general_configuration_mother import GeneralConfigurationMother
 
 
@@ -11,3 +12,10 @@ class TestGeneralConfiguration:
         expect(lambda: GeneralConfigurationMother.with_parameter(license="BSD")).to(
             raise_error(InvalidLicenseValue)
         )
+
+    def test_should_not_allow_to_create_general_configuration_with_invalid_python_version(
+        self,
+    ) -> None:
+        expect(
+            lambda: GeneralConfigurationMother.with_parameter(python_version="3.9")
+        ).to(raise_error(InvalidPythonVersionValue))
