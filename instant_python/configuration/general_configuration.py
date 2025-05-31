3d42eacc317f5261120e5804e839c7641f9675e5
dimanu-py <diegomtz126@gmail.com>
2025-06-01 01:05:48 +0200

    ✨ feat(configuration): validate passed license is supported by the application

--- instant_python/configuration/general_configuration.py
+++ instant_python/configuration/general_configuration.py
@@ -1,5 +1,9 @@
 from dataclasses import dataclass
 
+from instant_python.configuration.invalid_license_value import InvalidLicenseValue
+
+SUPPORTED_LICENSES = ["MIT", "Apache", "GPL"]
+
 
 @dataclass
 class GeneralConfiguration:
@@ -11,3 +15,10 @@ class GeneralConfiguration:
     license: str
     python_version: str
     dependency_manager: str
+
+    def __post_init__(self) -> None:
+        self._ensure_license_is_supported()
+
+    def _ensure_license_is_supported(self) -> None:
+        if self.license not in SUPPORTED_LICENSES:
+            raise InvalidLicenseValue(self.license)
