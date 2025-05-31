7ff810fcb9853908208e650384b5d2e15122acdc
dimanu-py <diegomtz126@gmail.com>
2025-06-01 01:42:31 +0200

    🧪 test(configuration): add test to validate correct instantiation of general configuration

--- test/configuration/test_general_configuration.py
+++ test/configuration/test_general_configuration.py
@@ -1,6 +1,8 @@
 from expects import expect, raise_error
 
-from instant_python.configuration.invalid_dependency_manager_value import InvalidDependencyManagerValue
+from instant_python.configuration.invalid_dependency_manager_value import (
+    InvalidDependencyManagerValue,
+)
 from instant_python.configuration.invalid_license_value import InvalidLicenseValue
 from instant_python.configuration.invalid_version_value import InvalidPythonVersionValue
 from test.configuration.general_configuration_mother import GeneralConfigurationMother
@@ -20,10 +22,15 @@ class TestGeneralConfiguration:
         expect(
             lambda: GeneralConfigurationMother.with_parameter(python_version="3.9")
         ).to(raise_error(InvalidPythonVersionValue))
-        
+
     def test_should_not_allow_to_create_general_configuration_with_invalid_dependency_manager(
         self,
     ) -> None:
         expect(
             lambda: GeneralConfigurationMother.with_parameter(dependency_manager="pip")
         ).to(raise_error(InvalidDependencyManagerValue))
+
+    def test_should_allow_to_create_general_configuration_with_valid_parameters(
+        self,
+    ) -> None:
+        GeneralConfigurationMother.any()
