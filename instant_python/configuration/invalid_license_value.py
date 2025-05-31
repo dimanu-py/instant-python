7a2f3c969442fed4d5410c26b842fd7b7b7d144a
dimanu-py <diegomtz126@gmail.com>
2025-06-01 01:04:10 +0200

    ✨ feat(configuration): create application error when invalid license is passed

---
+++ instant_python/configuration/invalid_license_value.py
@@ -0,0 +1,19 @@
+from instant_python.configuration.general_configuration import SUPPORTED_LICENSES
+from instant_python.errors.application_error import ApplicationError
+from instant_python.errors.error_types import ErrorTypes
+
+
+class InvalidLicenseValue(ApplicationError):
+    def __init__(self, value: str) -> None:
+        self._message = (
+            f"Invalid license: {value}. Allowed values are {SUPPORTED_LICENSES}."
+        )
+        super().__init__(self._message)
+
+    @property
+    def type(self) -> str:
+        return ErrorTypes.CONFIGURATION.value
+
+    @property
+    def message(self) -> str:
+        return self._message
