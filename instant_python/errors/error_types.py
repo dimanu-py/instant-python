b60121c43da3a462171f32503c91fec2edff6d75
dimanu-py <diegomtz126@gmail.com>
2025-06-01 01:03:48 +0200

    ✨ feat(errors): add configuration error to possible error types

--- instant_python/errors/error_types.py
+++ instant_python/errors/error_types.py
@@ -2,5 +2,6 @@ from enum import Enum
 
 
 class ErrorTypes(str, Enum):
-	INSTALLER = "installer_error"
-	GENERATOR = "project_generator_error"
+    INSTALLER = "installer_error"
+    GENERATOR = "project_generator_error"
+    CONFIGURATION = "configuration_error"
