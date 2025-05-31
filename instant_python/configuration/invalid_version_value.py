d0e267612c4ce2060052c27fa8a1aadf628b4c9c
dimanu-py <diegomtz126@gmail.com>
2025-06-01 01:08:41 +0200

    ✨ feat(configuration): add InvalidPythonVersionValue error for unsupported Python versions

---
+++ instant_python/configuration/invalid_version_value.py
@@ -0,0 +1,17 @@
+from instant_python.configuration.general_configuration import SUPPORTED_PYTHON_VERSIONS
+from instant_python.errors.application_error import ApplicationError
+from instant_python.errors.error_types import ErrorTypes
+
+
+class InvalidPythonVersionValue(ApplicationError):
+    def __init__(self, value: str) -> None:
+        self._message = f"Invalid Python version: {value}. Allowed versions are {SUPPORTED_PYTHON_VERSIONS}."
+        super().__init__(self._message)
+
+    @property
+    def type(self) -> str:
+        return ErrorTypes.CONFIGURATION.value
+
+    @property
+    def message(self) -> str:
+        return self._message
