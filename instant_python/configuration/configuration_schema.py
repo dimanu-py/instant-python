042e8fb62e0e8890572450f5441c0b269ea73ac3
dimanu-py <diegomtz126@gmail.com>
2025-06-01 12:05:07 +0200

    ✨ feat(configuration): add ConfigurationSchema to encapsulate general, dependency, template, and git configurations

---
+++ instant_python/configuration/configuration_schema.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+
+from instant_python.configuration.dependency.dependency_configuration import (
+    DependencyConfiguration,
+)
+from instant_python.configuration.general.general_configuration import (
+    GeneralConfiguration,
+)
+from instant_python.configuration.git.git_configuration import GitConfiguration
+from instant_python.configuration.template.template_configuration import (
+    TemplateConfiguration,
+)
+
+
+@dataclass
+class ConfigurationSchema:
+    general: GeneralConfiguration
+    dependency: DependencyConfiguration
+    template: TemplateConfiguration
+    git: GitConfiguration
+
+    def to_primitives(self) -> dict[str, dict]:
+        return {
+            "general": self.general.to_primitives(),
+            "dependency": self.dependency.to_primitives(),
+            "template": self.template.to_primitives(),
+            "git": self.git.to_primitives(),
+        }
