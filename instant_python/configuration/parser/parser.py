78aeef9a7329cdd3194082c322d3442c157c6785
dimanu-py <diegomtz126@gmail.com>
2025-06-01 14:33:49 +0200

    ✨ feat(configuration): handle EmptyConfigurationNotAllowed error for empty config files

--- instant_python/configuration/parser/parser.py
+++ instant_python/configuration/parser/parser.py
@@ -1,7 +1,10 @@
 import yaml
 
 from instant_python.configuration.configuration_schema import ConfigurationSchema
-from instant_python.configuration.parser.configuration_file_not_found import ConfigurationFileNotFound
+from instant_python.configuration.parser.configuration_file_not_found import (
+    ConfigurationFileNotFound,
+)
+from instant_python.configuration.parser.empty_configuration_not_allowed import EmptyConfigurationNotAllowed
 
 
 class Parser:
@@ -9,6 +12,11 @@ class Parser:
     def parse(cls, config_file_path: str) -> ConfigurationSchema:
         try:
             with open(config_file_path, "r") as file:
-                return yaml.safe_load(file)
+                content = yaml.safe_load(file)
         except FileNotFoundError:
             raise ConfigurationFileNotFound(config_file_path)
+
+        if not content:
+            raise EmptyConfigurationNotAllowed()
+
+        return content
