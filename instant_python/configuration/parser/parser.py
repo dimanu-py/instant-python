ce08d871295affa1fc92494269c35192ca7b4fbd
dimanu-py <diegomtz126@gmail.com>
2025-06-01 14:38:11 +0200

    ♻️ refactor(configuration): extract semantic method to encapsulate reading configuration file

--- instant_python/configuration/parser/parser.py
+++ instant_python/configuration/parser/parser.py
@@ -1,22 +1,29 @@
+from typing import Any
+
 import yaml
 
 from instant_python.configuration.configuration_schema import ConfigurationSchema
 from instant_python.configuration.parser.configuration_file_not_found import (
     ConfigurationFileNotFound,
 )
-from instant_python.configuration.parser.empty_configuration_not_allowed import EmptyConfigurationNotAllowed
+from instant_python.configuration.parser.empty_configuration_not_allowed import (
+    EmptyConfigurationNotAllowed,
+)
 
 
 class Parser:
     @classmethod
     def parse(cls, config_file_path: str) -> ConfigurationSchema:
+        content = cls._get_config_file_content(config_file_path)
+        return content
+
+    @classmethod
+    def _get_config_file_content(cls, config_file_path: str) -> dict[str, dict]:
         try:
             with open(config_file_path, "r") as file:
                 content = yaml.safe_load(file)
         except FileNotFoundError:
             raise ConfigurationFileNotFound(config_file_path)
-
         if not content:
             raise EmptyConfigurationNotAllowed()
-
         return content
