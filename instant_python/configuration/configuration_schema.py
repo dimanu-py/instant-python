1156aad4ea1ab844dacb77e94068205451db674d
dimanu-py <diegomtz126@gmail.com>
2025-06-01 16:33:04 +0200

    ♻️ refactor(configuration): temporarily set dependencies, template and git configs to not needed when initializing ConfigurationSchema to be able to test it step by step

--- instant_python/configuration/configuration_schema.py
+++ instant_python/configuration/configuration_schema.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 from instant_python.configuration.dependency.dependency_configuration import (
     DependencyConfiguration,
@@ -15,9 +15,9 @@ from instant_python.configuration.template.template_configuration import (
 @dataclass
 class ConfigurationSchema:
     general: GeneralConfiguration
-    dependencies: list[DependencyConfiguration]
-    template: TemplateConfiguration
-    git: GitConfiguration
+    dependencies: list[DependencyConfiguration] = field(init=False)
+    template: TemplateConfiguration = field(init=False)
+    git: GitConfiguration = field(init=False)
 
     def to_primitives(self) -> dict[str, dict]:
         return {
