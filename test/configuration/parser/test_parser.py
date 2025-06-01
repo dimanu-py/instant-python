3035ebbc17333cf04854957e1052bd7848f83990
dimanu-py <diegomtz126@gmail.com>
2025-06-01 17:22:18 +0200

    ♻️ refactor(tests): update config file path handling to remove file extension

--- test/configuration/parser/test_parser.py
+++ test/configuration/parser/test_parser.py
@@ -19,38 +19,38 @@ from instant_python.configuration.parser.parser import Parser
 class TestParser:
     @staticmethod
     def _build_config_file_path(file_name: str) -> str:
-        return str(Path(__file__).parent / "resources" / file_name)
+        return str(Path(__file__).parent / "resources" / f"{file_name}.yml")
 
     def test_should_raise_error_if_config_file_is_not_found(self) -> None:
-        config_file_path = "non_existent_config_file.yml"
+        config_file_path = "non_existent_config_file"
 
         expect(lambda: Parser.parse(config_file_path)).to(
             raise_error(ConfigurationFileNotFound)
         )
 
     def test_should_load_config_file_when_exists(self) -> None:
-        config_file_path = self._build_config_file_path("config.yml")
+        config_file_path = self._build_config_file_path("config")
 
         config = Parser.parse(config_file_path)
 
         expect(config).to_not(be_none)
 
     def test_should_raise_error_if_config_file_is_empty(self) -> None:
-        config_file_path = self._build_config_file_path("empty_config.yml")
+        config_file_path = self._build_config_file_path("empty_config")
 
         expect(lambda: Parser.parse(config_file_path)).to(
             raise_error(EmptyConfigurationNotAllowed)
         )
 
     def test_should_raise_error_if_config_keys_are_not_present(self) -> None:
-        config_file_path = self._build_config_file_path("missing_keys_config.yml")
+        config_file_path = self._build_config_file_path("missing_keys_config")
 
         expect(lambda: Parser.parse(config_file_path)).to(
             raise_error(ConfigKeyNotPresent)
         )
 
     def test_should_parse_general_configuration_key(self) -> None:
-        config_file_path = self._build_config_file_path("config.yml")
+        config_file_path = self._build_config_file_path("config")
 
         config = Parser.parse(config_file_path)
 
@@ -67,7 +67,7 @@ class TestParser:
         expect(config.general).to(equal(expected_general_config))
 
     def test_should_raise_error_if_general_configuration_has_missing_mandatory_fields(self) -> None:
-        config_file_path = self._build_config_file_path("missing_general_fields_config.yml")
+        config_file_path = self._build_config_file_path("missing_general_fields_config")
         
         expect(lambda: Parser.parse(config_file_path)).to(
             raise_error(MissingMandatoryFields)
