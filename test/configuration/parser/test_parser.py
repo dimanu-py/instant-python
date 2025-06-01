bf689077b5e25b2a3ddcc76282dde5fa99b09a85
dimanu-py <diegomtz126@gmail.com>
2025-06-01 14:16:49 +0200

    🧪 test(configuration): add red unit test to verify error is raised if config file is not found

---
+++ test/configuration/parser/test_parser.py
@@ -0,0 +1,12 @@
+from expects import expect, raise_error
+
+from instant_python.configuration.parser.parser import Parser
+
+
+class TestParser:
+    def test_should_raise_error_if_config_file_is_not_found(self) -> None:
+        config_file_path = "non_existent_config_file.yml"
+
+        expect(lambda: Parser.parse(config_file_path)).to(
+            raise_error(ConfigurationFileNotFound)
+        )
