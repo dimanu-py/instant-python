e2d0c2cc2daef48127aeca99475335a25b89b98d
dimanu-py <diegomtz126@gmail.com>
2025-06-02 09:25:59 +0200

    🐛 fix(error): correct message formatting in NotDevDependencyIncludedInGroup exception

--- instant_python/configuration/dependency/not_dev_dependency_included_in_group.py
+++ instant_python/configuration/dependency/not_dev_dependency_included_in_group.py
@@ -4,7 +4,7 @@ from instant_python.errors.error_types import ErrorTypes
 
 class NotDevDependencyIncludedInGroup(ApplicationError):
     def __init__(self, dependency_name: str, dependency_group: str) -> None:
-        self._message = f"Dependency '{dependency_name} has been included in group '{dependency_group}' but it is not a development dependency. Please ensure that only development dependencies are included in groups."
+        self._message = f"Dependency '{dependency_name}' has been included in group '{dependency_group}' but it is not a development dependency. Please ensure that only development dependencies are included in groups."
         super().__init__(self._message)
 
     @property
