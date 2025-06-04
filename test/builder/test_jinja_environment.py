bc9719a7c7562dcddd2e6dda5b353b2980658bec
dimanu-py <diegomtz126@gmail.com>
2025-06-04 09:19:26 +0200

    🧪 test(builder): add unit test to verify a template can be rendered  through the environment

--- test/builder/test_jinja_environment.py
+++ test/builder/test_jinja_environment.py
@@ -1,4 +1,4 @@
-from expects import expect, be_true, be_none
+from expects import expect, be_true, be_none, equal
 
 from instant_python.builder.jinja_environment import JinjaEnvironment
 
@@ -16,3 +16,10 @@ class TestJinjaEnvironment:
 
         expect("is_in" in jinja_env._env.filters).to(be_true)
         expect("compute_base_path" in jinja_env._env.filters).to(be_true)
+
+    def test_should_render_template(self) -> None:
+        jinja_env = JinjaEnvironment(package_name="test", template_dir="templates")
+
+        rendered_content = jinja_env.render_template("test_template.j2", {"name": "World"})
+
+        expect(rendered_content).to(equal("Hello World!"))
