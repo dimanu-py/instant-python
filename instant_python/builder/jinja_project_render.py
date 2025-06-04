70014cdef13c5b5f9d3b10aaad4ec8a672652a28
dimanu-py <diegomtz126@gmail.com>
2025-06-04 15:50:25 +0200

    ✨ feat(builder): implement "get_project" method in JinjaProjectRender class

--- instant_python/builder/jinja_project_render.py
+++ instant_python/builder/jinja_project_render.py
@@ -1,3 +1,5 @@
+import yaml
+
 from instant_python.builder.jinja_environment import JinjaEnvironment
 from instant_python.configuration.configuration_schema import ConfigurationSchema
 
@@ -7,4 +9,7 @@ class JinjaProjectRender:
         self._jinja_environment = jinja_environment
 
     def get_project(self, context_config: ConfigurationSchema) -> dict[str, list[dict]]:
-        raise NotImplementedError()
+        template_type = context_config.template.name
+        template_name = f"project_structure/{template_type}/main_structure.yml.j2"
+        raw_project_structure = self._jinja_environment.render_template(name=template_name, context=context_config.to_primitives())
+        return yaml.safe_load(raw_project_structure)
