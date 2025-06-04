b1926decc244dba3cc50ee6bbc3ae8de9abcf820
dimanu-py <diegomtz126@gmail.com>
2025-06-04 12:54:35 +0200

    ♻️ refactor(builder): pass package name and template directory to jinja environment to be able to differentiate between production templates and test templates

--- instant_python/builder/jinja_environment.py
+++ instant_python/builder/jinja_environment.py
@@ -4,9 +4,9 @@ from instant_python.project_generator.jinja_custom_filters import is_in, compute
 
 
 class JinjaEnvironment:
-    def __init__(self) -> None:
+    def __init__(self, package_name: str, template_directory: str) -> None:
         self._env = Environment(
-	        loader=PackageLoader("instant_python", "templates"),
+	        loader=PackageLoader(package_name, template_directory),
 	        trim_blocks=True,
 	        lstrip_blocks=True,
         )
--- test/builder/test_jinja_environment.py
+++ test/builder/test_jinja_environment.py
@@ -5,20 +5,20 @@ from instant_python.builder.jinja_environment import JinjaEnvironment
 
 class TestJinjaEnvironment:
     def test_should_initialize_jinja_environment(self) -> None:
-        jinja_env = JinjaEnvironment()
+        jinja_env = JinjaEnvironment(package_name="test", template_directory="builder/resources")
 
         expect(jinja_env._env).not_to(be_none)
         expect(jinja_env._env.trim_blocks).to(be_true)
         expect(jinja_env._env.lstrip_blocks).to(be_true)
 
     def test_should_register_custom_filters(self) -> None:
-        jinja_env = JinjaEnvironment()
+        jinja_env = JinjaEnvironment(package_name="test", template_directory="builder/resources")
 
         expect("is_in" in jinja_env._env.filters).to(be_true)
         expect("compute_base_path" in jinja_env._env.filters).to(be_true)
 
     def test_should_render_template(self) -> None:
-        jinja_env = JinjaEnvironment(package_name="test", template_dir="templates")
+        jinja_env = JinjaEnvironment(package_name="test", template_directory="builder/resources")
 
         rendered_content = jinja_env.render_template("test_template.j2", {"name": "World"})
 
