6468bae706972273a9d58c1685d37e87ab4579a2
dimanu-py <diegomtz126@gmail.com>
2025-06-09 07:31:54 +0200

    ✨ feat(project-creator): add '__repr__' method to BoilerplateFile class

--- instant_python/project_creator/boilerplate_file.py
+++ instant_python/project_creator/boilerplate_file.py
@@ -7,5 +7,8 @@ class BoilerplateFile(Node):
     def __init__(self, name: str, extension: str) -> None:
         self._file_name = f"{name.split('/')[-1]}{extension}"
 
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(name={self._file_name})"
+
     def create(self, base_path: Path) -> None:
         raise NotImplementedError
