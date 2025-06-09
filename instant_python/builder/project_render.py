0f848450a959398dac64b925f21a58854994e611
dimanu-py <diegomtz126@gmail.com>
2025-06-09 07:08:59 +0200

    ♻️ refactor(builder): remove old project_render.py and test

--- instant_python/builder/project_render.py
+++
@@ -1,6 +0,0 @@
-from instant_python.builder.jinja_environment import JinjaEnvironment
-
-
-class ProjectRender:
-    def __init__(self, jinja_environment: JinjaEnvironment) -> None:
-        self._jinja_environment = jinja_environment
