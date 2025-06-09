4fa6143117603f7c0955e244a9a56264fda2acbd
dimanu-py <diegomtz126@gmail.com>
2025-06-09 07:36:44 +0200

    🧪 test(project-creator): verify boilerplate file gets created at path with correct content

--- test/project_creator/test_boilerplate_file.py
+++ test/project_creator/test_boilerplate_file.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from instant_python.project_creator.boilerplate_file import BoilerplateFile
 
 
@@ -6,3 +8,11 @@ class TestBoilerplateFile:
         file = BoilerplateFile(name="exceptions/domain_error", extension=".py")
 
         assert file._file_name == "domain_error.py"
+
+    def test_should_create_file_at_specified_file(self) -> None:
+        file = BoilerplateFile(name="exceptions/domain_error", extension=".py")
+
+        file.create(base_path=Path(__file__).parent)
+
+        file_path = Path(__file__).parent / "domain_error.py"
+        assert file_path.exists()
