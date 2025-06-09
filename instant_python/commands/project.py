f418ee7cd9410ef6df9cce6856e12f43e5486bd0
dimanu-py <diegomtz126@gmail.com>
2025-06-09 07:15:24 +0200

    ♻️ refactor(commands): add type hint to project command

--- instant_python/commands/project.py
+++ instant_python/commands/project.py
@@ -8,9 +8,9 @@ app = typer.Typer()
 
 
 @app.command("new", help="Create a new project")
-def create_new_project(config_file: str = typer.Option(..., "--config", "-c", help="Path to yml configuration file")):
+def create_new_project(config_file: str = typer.Option(..., "--config", "-c", help="Path to yml configuration file")) -> None:
     configuration = Parser.parse(config_file_path=config_file)
-    
+
     environment = JinjaEnvironment(package_name="instant_python", template_directory="templates")
     project_structure = JinjaProjectRender(jinja_environment=environment)
     project_structure.render_project_structure(context_config=configuration, template_base_dir="project_structure")
