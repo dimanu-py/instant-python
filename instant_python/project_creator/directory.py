5756f8e92799b584c5fd8acc733979717c3b4be4
dimanu-py <diegomtz126@gmail.com>
2025-06-10 08:20:28 +0200

    ✨ feat(project-creator): create Directory class with basic attributes

---
+++ instant_python/project_creator/directory.py
@@ -0,0 +1,11 @@
+from instant_python.project_creator.node import Node
+
+
+class Directory:
+    def __init__(self, name: str, is_python: bool, children: list[Node]) -> None:
+        self._name = name
+        self._is_python = is_python
+        self._children = children
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(name={self._name}, is_python={self._is_python}, children={self._children})"
