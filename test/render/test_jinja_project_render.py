efed0d2a3ecbf8c17f7c3bfd589384e7320b775c
dimanu-py <diegomtz126@gmail.com>
2025-06-10 23:51:15 +0200

    ♻️ refactor(render): modify tests to use approvaltest and don't need expected project json files

--- test/render/test_jinja_project_render.py
+++ test/render/test_jinja_project_render.py
@@ -2,7 +2,8 @@ import json
 from pathlib import Path
 
 import pytest
-from expects import expect, equal
+from approvaltests import verify
+from approvaltests.namer import NamerFactory
 
 from instant_python.render.jinja_environment import JinjaEnvironment
 from instant_python.render.jinja_project_render import JinjaProjectRender
@@ -15,30 +16,19 @@ class TestJinjaProjectRender:
         self._project_render = JinjaProjectRender(jinja_environment=jinja_environment)
 
     @pytest.mark.parametrize(
-        "config_path, expected_path",
+        "config_path",
         [
-            pytest.param("clean_architecture_config.yml", "clean_architecture/expected_project.json", id="clean_architecture"),
-            pytest.param("domain_driven_design_config.yml", "domain_driven_design/expected_project.json", id="domain_driven_design"),
-            pytest.param(
-                "standard_project_with_git_config.yml", "standard_project/expected_project_with_git.json", id="standard_project_with_git"
-            ),
-            pytest.param(
-                "standard_project_with_dependency_config.yml",
-                "standard_project/expected_project_with_dependency.json",
-                id="standard_project_with_dependency",
-            ),
+            pytest.param("clean_architecture_config.yml", id="clean_architecture"),
+            pytest.param("domain_driven_design_config.yml", id="domain_driven_design"),
+            pytest.param("standard_project_with_git_config.yml", id="standard_project_with_git"),
+            pytest.param("standard_project_with_dependency_config.yml", id="standard_project_with_dependency"),
         ],
     )
-    def test_should_render_template_for(self, config_path: str, expected_path: str) -> None:
+    def test_should_render_template_for(self, config_path: str) -> None:
         resources_path = str(Path(__file__).parent / "resources")
         configuration = Parser.parse(f"{resources_path}/{config_path}")
 
         rendered_project = self._project_render.render_project_structure(context_config=configuration, template_base_dir="resources")
 
-        expected_project = self._load_expected_project(f"{resources_path}/{expected_path}")
-        expect(rendered_project).to(equal(expected_project))
-
-    @staticmethod
-    def _load_expected_project(path: str) -> dict:
-        with open(path, "r") as file:
-            return json.load(file)
+        rendered_project_json = json.dumps(rendered_project, indent=2, sort_keys=True)
+        verify(rendered_project_json, options=NamerFactory.with_parameters(config_path))
