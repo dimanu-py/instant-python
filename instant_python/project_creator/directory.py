2f840fa54bcac28bc23c09dba67a7c9f6ad711c6
dimanu-py <diegomtz126@gmail.com>
2025-06-10 08:37:31 +0200

    ✨ feat(project-creator): when directory is defined as python module, create '__init__' file inside

--- instant_python/project_creator/directory.py
+++ instant_python/project_creator/directory.py
@@ -4,14 +4,20 @@ from instant_python.project_creator.node import Node
 
 
 class Directory:
+    _INIT_FILE = "__init__.py"
+
     def __init__(self, name: str, is_python: bool, children: list[Node]) -> None:
         self._name = name
-        self._is_python = is_python
+        self._is_python_module = is_python
         self._children = children
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__name__}(name={self._name}, is_python={self._is_python}, children={self._children})"
+        return f"{self.__class__.__name__}(name={self._name}, is_python={self._is_python_module}, children={self._children})"
 
     def create(self, base_path: Path) -> None:
         path = base_path / self._name
         path.mkdir(parents=True, exist_ok=True)
+
+        if self._is_python_module:
+            init_file_path = path / self._INIT_FILE
+            init_file_path.touch(exist_ok=True)
