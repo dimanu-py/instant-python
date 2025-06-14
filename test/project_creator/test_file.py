9004c57dced276315e6d29cfc8248ed6585ea9a3
dimanu-py <diegomtz126@gmail.com>
2025-06-14 12:48:29 +0200

    ♻️ refactor(project-creator): extract setup_method for file tests to clean up file creation

--- test/project_creator/test_file.py
+++ test/project_creator/test_file.py
@@ -4,6 +4,9 @@ from instant_python.project_creator.file import File
 
 
 class TestFile:
+    def setup_method(self) -> None:
+        self._file = File(name="exceptions/domain_error", extension=".py")
+
     def teardown_method(self) -> None:
         file_path = Path(__file__).parent / "domain_error.py"
 
@@ -11,22 +14,16 @@ class TestFile:
             file_path.unlink()
 
     def test_should_extract_file_name(self) -> None:
-        file = File(name="exceptions/domain_error", extension=".py")
-
-        assert file._file_name == "domain_error.py"
+        assert self._file._file_name == "domain_error.py"
 
     def test_should_create_file_at_specified_path(self) -> None:
-        file = File(name="exceptions/domain_error", extension=".py")
-
-        file.create(base_path=Path(__file__).parent)
+        self._file.create(base_path=Path(__file__).parent)
 
         file_path = Path(__file__).parent / "domain_error.py"
         assert file_path.exists()
 
     def test_should_fill_file_with_template_content(self) -> None:
-        file = File(name="exceptions/domain_error", extension=".py")
-
-        file.create(base_path=Path(__file__).parent)
+        self._file.create(base_path=Path(__file__).parent)
 
         file_path = Path(__file__).parent / "domain_error.py"
         assert file_path.read_text() == "class DomainError(Exception):\n    pass\n"
