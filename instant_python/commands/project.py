cfbb2a20d1af7ba934e1793b04448c7fea0d7a64
dimanu-py <diegomtz126@gmail.com>
2025-06-14 12:23:40 +0200

    ♻️ refactor(commands): change how new project command is handled using directly FyleSystem class

--- instant_python/commands/project.py
+++ instant_python/commands/project.py
@@ -1,8 +1,8 @@
 import typer
 
-from instant_python.render.jinja_environment import JinjaEnvironment
-from instant_python.render.jinja_project_render import JinjaProjectRender
 from instant_python.configuration.parser.parser import Parser
+from instant_python.project_creator.file_system import FileSystem
+from instant_python.render.jinja_environment import JinjaEnvironment
 
 app = typer.Typer()
 
@@ -10,7 +10,9 @@ app = typer.Typer()
 @app.command("new", help="Create a new project")
 def create_new_project(config_file: str = typer.Option(..., "--config", "-c", help="Path to yml configuration file")) -> None:
     configuration = Parser.parse(config_file_path=config_file)
-
     environment = JinjaEnvironment(package_name="instant_python", template_directory="templates")
     project_structure = JinjaProjectRender(jinja_environment=environment)
     project_structure.render_project_structure(context_config=configuration, template_base_dir="project_structure")
+
+    file_system = FileSystem(jinja_environment=environment)
+    file_system.create_folders_and_files(context_config=configuration, template_base_dir="project_structure")
