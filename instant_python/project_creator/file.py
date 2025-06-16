138d979383958ca764d2ee90dd4b6a3d10bf7834
dimanu-py <diegomtz126@gmail.com>
2025-06-16 09:23:00 +0200

    ♻️ refactor(project-creator): when creating a File save its path to be able to recover it when filling it

--- instant_python/project_creator/file.py
+++ instant_python/project_creator/file.py
@@ -6,11 +6,12 @@ from instant_python.project_creator.node import Node
 class File(Node):
     def __init__(self, name: str, extension: str) -> None:
         self._file_name = f"{name.split('/')[-1]}{extension}"
+        self._file_path = None
         self._template_path = f"boilerplate/{name}{extension}"
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}(name={self._file_name})"
 
     def create(self, base_path: Path) -> None:
-        file_path = base_path / self._file_name
-        file_path.touch(exist_ok=True)
+        self._file_path = base_path / self._file_name
+        self._file_path.touch(exist_ok=True)
