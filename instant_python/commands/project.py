fb079708bcb812de20a8465999f960bf4709d317
dimanu-py <diegomtz126@gmail.com>
2025-06-17 08:03:56 +0200

    ♻️ refactor(commands): update project command to use new "write_on_disk" file system method to create the project on disk

--- instant_python/commands/project.py
+++ instant_python/commands/project.py
@@ -3,6 +3,7 @@ import typer
 from instant_python.configuration.parser.parser import Parser
 from instant_python.project_creator.file_system import FileSystem
 from instant_python.render.jinja_environment import JinjaEnvironment
+from instant_python.render.jinja_project_renderer import JinjaProjectRenderer
 
 app = typer.Typer()
 
@@ -12,8 +13,16 @@ def create_new_project(config_file: str = typer.Option(..., "--config", "-c", he
     configuration = Parser.parse(config_file_path=config_file)
     environment = JinjaEnvironment(package_name="instant_python", template_directory="templates")
 
-    file_system = FileSystem(jinja_environment=environment)
-    file_system.create_folders_and_files(context_config=configuration, template_base_dir="project_structure")
+    project_renderer = JinjaProjectRenderer(jinja_environment=environment)
+    project_structure = project_renderer.render_project_structure(
+        context_config=configuration,
+        template_base_dir="project_structure",
+    )
+    file_system = FileSystem(project_structure=project_structure)
+    file_system.write_on_disk(
+        file_renderer=environment,
+        context=configuration,
+    )
 
 
 if __name__ == "__main__":
