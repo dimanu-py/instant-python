ffea41b0bced8eb7cce46853bd5a9d87f8807a7e
dimanu-py <diegomtz126@gmail.com>
2025-06-18 10:53:34 +0200

    ✨ feat(dependency-manager): add _install method to UvDependencyManager

--- instant_python/dependency_manager/uv_dependency_manager.py
+++ instant_python/dependency_manager/uv_dependency_manager.py
@@ -1,3 +1,6 @@
 class UvDependencyManager:
     def __init__(self, project_directory: str) -> None:
         self._project_directory = project_directory
+
+    def _install(self) -> None:
+        raise NotImplementedError
