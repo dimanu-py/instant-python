9d0fa89b21564a43bcc8f0032f06b643410169da
dimanu-py <diegomtz126@gmail.com>
2025-06-18 15:50:32 +0200

    🧪 test(dependency-manager): add test to verify python version gets installed

--- test/dependency_manager/test_uv_dependency_manager.py
+++ test/dependency_manager/test_uv_dependency_manager.py
@@ -11,3 +11,12 @@ class TestUvDependencyManager:
         uv_dependency_manager._install()
 
         uv_dependency_manager.expect_to_have_been_called_with(command="curl -LsSf https://astral.sh/uv/install.sh | sh")
+
+    def test_should_install_specific_pyton_version(self) -> None:
+        project_directory = os.getcwd()
+        uv_dependency_manager = MockUvDependencyManager(project_directory=project_directory)
+        python_version = "3.12"
+
+        uv_dependency_manager._install_python(version=f"{python_version}")
+
+        uv_dependency_manager.expect_to_have_been_called_with(command=f"~/.local/bin/uv python install {python_version}")
