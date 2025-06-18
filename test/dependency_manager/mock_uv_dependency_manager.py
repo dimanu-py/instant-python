a2bfde1699ea3378693622c13778a6e025d7d155
dimanu-py <diegomtz126@gmail.com>
2025-06-18 15:44:28 +0200

    🧪 test(dependency-manager): implement "expect_to_have_been_called_with" method to verify command execution

--- test/dependency_manager/mock_uv_dependency_manager.py
+++ test/dependency_manager/mock_uv_dependency_manager.py
@@ -1,3 +1,5 @@
+from expects import expect, contain
+
 from instant_python.dependency_manager.uv_dependency_manager import UvDependencyManager
 
 
@@ -5,3 +7,6 @@ class MockUvDependencyManager(UvDependencyManager):
     def __init__(self, project_directory: str) -> None:
         super().__init__(project_directory)
         self._commands = []
+
+    def expect_to_have_been_called_with(self, command: str) -> None:
+        expect(self._commands).to(contain(command))
