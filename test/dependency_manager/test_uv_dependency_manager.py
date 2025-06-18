8e7137f58b302487b2cb0232702fe9cd0433c84e
dimanu-py <diegomtz126@gmail.com>
2025-06-18 15:54:56 +0200

    🧪 test(dependency-manager): clean up test setup by introducing setup_method with common instantiation of dependency manager

--- test/dependency_manager/test_uv_dependency_manager.py
+++ test/dependency_manager/test_uv_dependency_manager.py
@@ -4,19 +4,17 @@ from test.dependency_manager.mock_uv_dependency_manager import MockUvDependencyM
 
 
 class TestUvDependencyManager:
+    def setup_method(self) -> None:
+        self._uv_dependency_manager = MockUvDependencyManager(project_directory=os.getcwd())
+
     def test_should_install_uv(self) -> None:
-        project_directory = os.getcwd()
-        uv_dependency_manager = MockUvDependencyManager(project_directory=project_directory)
+        self._uv_dependency_manager._install()
 
-        uv_dependency_manager._install()
-
-        uv_dependency_manager.expect_to_have_been_called_with(command="curl -LsSf https://astral.sh/uv/install.sh | sh")
+        self._uv_dependency_manager.expect_to_have_been_called_with(command="curl -LsSf https://astral.sh/uv/install.sh | sh")
 
     def test_should_install_specific_pyton_version(self) -> None:
-        project_directory = os.getcwd()
-        uv_dependency_manager = MockUvDependencyManager(project_directory=project_directory)
         python_version = "3.12"
 
-        uv_dependency_manager._install_python(version=f"{python_version}")
+        self._uv_dependency_manager._install_python(version=f"{python_version}")
 
-        uv_dependency_manager.expect_to_have_been_called_with(command=f"~/.local/bin/uv python install {python_version}")
+        self._uv_dependency_manager.expect_to_have_been_called_with(command=f"~/.local/bin/uv python install {python_version}")
