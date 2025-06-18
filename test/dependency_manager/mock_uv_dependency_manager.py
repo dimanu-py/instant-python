2fabc9fdbdd173fad3e03572cdb49c29433e3d5a
dimanu-py <diegomtz126@gmail.com>
2025-06-18 15:46:59 +0200

    🧪 test(dependency-manager): override "_run_command" method to not actually execute the command and instead store it as a called command

--- test/dependency_manager/mock_uv_dependency_manager.py
+++ test/dependency_manager/mock_uv_dependency_manager.py
@@ -1,3 +1,5 @@
+from typing import override
+
 from expects import expect, contain
 
 from instant_python.dependency_manager.uv_dependency_manager import UvDependencyManager
@@ -7,6 +9,10 @@ class MockUvDependencyManager(UvDependencyManager):
     def __init__(self, project_directory: str) -> None:
         super().__init__(project_directory)
         self._commands = []
+
+    @override
+    def _run_command(self, command: str) -> None:
+        self._commands.append(command)
 
     def expect_to_have_been_called_with(self, command: str) -> None:
         expect(self._commands).to(contain(command))
