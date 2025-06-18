39dc66bdf5dde77b6f777d5d86c3746ab09aae77
dimanu-py <diegomtz126@gmail.com>
2025-06-18 15:42:11 +0200

    ✨ feat(dependency-manager): implement "_install" method delegating command execution to a helper "_run_command" method
    The idea of delegating to that helper command is to be able to mock it and verify the commands are called but not actually executed on the test

--- instant_python/dependency_manager/uv_dependency_manager.py
+++ instant_python/dependency_manager/uv_dependency_manager.py
@@ -1,6 +1,21 @@
+import subprocess
+
+
 class UvDependencyManager:
     def __init__(self, project_directory: str) -> None:
         self._project_directory = project_directory
 
     def _install(self) -> None:
-        raise NotImplementedError
+        print(">>> Installing uv...")
+        self._run_command(command="curl -LsSf https://astral.sh/uv/install.sh | sh")
+        print(">>> uv installed successfully")
+
+    def _run_command(self, command: str) -> None:
+        subprocess.run(
+            command,
+            shell=True,
+            check=True,
+            cwd=self._project_directory,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.PIPE,
+        )
