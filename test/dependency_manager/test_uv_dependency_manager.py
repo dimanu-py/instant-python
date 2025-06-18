fd7d227ba634f539363e0585bf46bfdc27773d3b
dimanu-py <diegomtz126@gmail.com>
2025-06-18 10:47:57 +0200

    🧪 test(dependency-manager): add test to verify install command executes command to install uv using curl

---
+++ test/dependency_manager/test_uv_dependency_manager.py
@@ -0,0 +1,11 @@
+import os
+
+
+class TestUvDependencyManager:
+    def test_should_install_uv(self) -> None:
+        project_directory = os.getcwd()
+        uv_dependency_manager = MockUvDependencyManager(project_directory=project_directory)
+
+        uv_dependency_manager._install()
+
+        uv_dependency_manager.expect_to_have_been_called_with(command="curl -LsSf https://astral.sh/uv/install.sh | sh")
