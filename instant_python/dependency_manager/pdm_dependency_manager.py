7a8292fc87c95c3daf4a6ac00b9b536f9d3c70f9
dimanu-py <diegomtz126@gmail.com>
2025-06-20 21:26:45 +0200

    ✨ feat(dependency-manager): implement concrete version of dependency manager  using pdm

---
+++ instant_python/dependency_manager/pdm_dependency_manager.py
@@ -0,0 +1,57 @@
+import subprocess
+
+from instant_python.dependency_manager.dependency_manager import DependencyManager
+from instant_python.errors.command_execution_error import CommandExecutionError
+
+
+class PdmDependencyManager(DependencyManager):
+    def __init__(self, project_directory: str) -> None:
+        super().__init__(project_directory)
+        self._pdm = "~/.local/bin/pdm"
+
+    def setup_environment(self, python_version: str, dependencies: list[dict]) -> None:
+        try:
+            self._install()
+            self._install_python(python_version)
+            self._install_dependencies(dependencies)
+        except subprocess.CalledProcessError as error:
+            raise CommandExecutionError(exit_code=error.returncode, stderr_output=error.stderr)
+
+    def _install(self) -> None:
+        print(">>> Installing pdm...")
+        self._run_command(command="curl -sSL https://pdm-project.org/install-pdm.py | python3 -")
+        print(">>> pdm installed successfully")
+
+    def _install_python(self, version: str) -> None:
+        print(f">>> Installing Python {version}...")
+        self._run_command(command=f"{self._pdm} python install {version}")
+        print(f">>> Python {version} installed successfully")
+
+    def _install_dependencies(self, dependencies: list[dict]) -> None:
+        self._create_virtual_environment()
+        print(">>> Installing dependencies...")
+        for dependency in dependencies:
+            command = self._build_dependency_install_command(dependency)
+            self._run_command(command)
+            print(f">>> Dependency {dependency} installed successfully")
+
+    def _build_dependency_install_command(self, dependency: dict[str, str]) -> str:
+        name = dependency["name"]
+        version = dependency["version"]
+        is_dev = dependency.get("is_dev", False)
+        group = dependency.get("group", None)
+
+        command = [f"{self._pdm} add"]
+
+        if is_dev:
+            command.append("--dev")
+        if group:
+            command.append(f"--group {group}")
+
+        dependency_spec = name if version == "latest" else f"{name}=={version}"
+        command.append(dependency_spec)
+
+        return " ".join(command)
+
+    def _create_virtual_environment(self) -> None:
+        self._run_command(f"{self._pdm} install")
