9d646dd2e808e764470cc270e6cfeb6e1bfcef6b
dimanu-py <diegomtz126@gmail.com>
2025-06-20 21:38:26 +0200

    ✨ feat(dependency-manager): implement factory method to encapsulate instantiation of dependency manager based on user selection

---
+++ instant_python/dependency_manager/dependency_manager_factory.py
@@ -0,0 +1,18 @@
+from instant_python.dependency_manager.dependency_manager import DependencyManager
+from instant_python.dependency_manager.pdm_dependency_manager import PdmDependencyManager
+from instant_python.dependency_manager.uv_dependency_manager import UvDependencyManager
+from instant_python.errors.unknown_dependency_manager_error import UnknownDependencyManagerError
+from instant_python.installer.managers import Managers
+
+
+class DependencyManagerFactory:
+    @staticmethod
+    def create(dependency_manager: str, project_directory: str) -> DependencyManager:
+        managers = {
+            Managers.UV: UvDependencyManager,
+            Managers.PDM: PdmDependencyManager,
+        }
+        try:
+            return managers[Managers(dependency_manager)](project_directory)
+        except KeyError:
+            raise UnknownDependencyManagerError(dependency_manager)
