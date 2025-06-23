fbc793e1ae12545870caa1fe7c3b04655843c5c7
dimanu-py <diegomtz126@gmail.com>
2025-06-23 15:16:18 +0200

    🧪 test(git): add test to verify git repository is not initialized if is not specified in config

---
+++ test/git/test_git_configurer.py
@@ -0,0 +1,13 @@
+import os
+
+from test.configuration.git.git_configuration_mother import GitConfigurationMother
+
+
+class TestGitConfigurer:
+    def test_should_not_initialize_git_repository_if_is_not_specified(self) -> None:
+        configuration = GitConfigurationMother.not_initialize()
+        git_configurer = MockGitConfigurer(project_directory=os.getcwd())
+
+        git_configurer.setup_repository(configuration=configuration)
+
+        git_configurer.expect_to_not_have_initialized_repository()
