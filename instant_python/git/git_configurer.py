6ef19e66041a2e7cc87f12d8610e0e8b4de4442d
dimanu-py <diegomtz126@gmail.com>
2025-06-23 15:20:14 +0200

    ✨ feat(git): add "setup_repository" method to GitConfigurer

--- instant_python/git/git_configurer.py
+++ instant_python/git/git_configurer.py
@@ -1,3 +1,9 @@
+from instant_python.configuration.git.git_configuration import GitConfiguration
+
+
 class GitConfigurer:
     def __init__(self, project_directory: str) -> None:
         self._project_directory = project_directory
+
+    def setup_repository(self, configuration: GitConfiguration) -> None:
+        raise NotImplementedError
