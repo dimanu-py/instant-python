9c9df5ec6c216f0180c394b615d65f93c354ced3
dimanu-py <diegomtz126@gmail.com>
2025-06-23 15:22:55 +0200

    🧪 test(git): implement assertion method to verify no command has been executed if git is not set to be initialized

--- test/git/mock_git_configurer.py
+++ test/git/mock_git_configurer.py
@@ -1,6 +1,12 @@
+from expects import expect, be_empty
+
 from instant_python.git.git_configurer import GitConfigurer
 
 
 class MockGitConfigurer(GitConfigurer):
     def __init__(self, project_directory: str) -> None:
         super().__init__(project_directory=project_directory)
+        self._commands = []
+
+    def expect_to_not_have_initialized_repository(self) -> None:
+        expect(self._commands).to(be_empty)
