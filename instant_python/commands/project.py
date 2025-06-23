798f7052624c373c14d0f778db38b7a82d704ef5
dimanu-py <diegomtz126@gmail.com>
2025-06-23 16:25:53 +0200

    ✨ feat(commands): rename new project command to "init" so the use is ipy init

--- instant_python/commands/project.py
+++ instant_python/commands/project.py
@@ -10,7 +10,7 @@ from instant_python.render.jinja_project_renderer import JinjaProjectRenderer
 app = typer.Typer()
 
 
-@app.command("new", help="Create a new project")
+@app.command("init", help="Create a new project")
 def create_new_project(
     config_file: str = typer.Option(..., "--config", "-c", help="Path to yml configuration file"),
 ) -> None:
