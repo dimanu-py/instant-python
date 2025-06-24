f0701448553b2029c766303c31de0bc6f77f2e7b
dimanu-py <diegomtz126@gmail.com>
2025-06-24 17:22:46 +0200

    ✨ feat(commands): allow the option of passing a custom template to generate a project with a custom structure

--- instant_python/commands/project.py
+++ instant_python/commands/project.py
@@ -4,6 +4,7 @@ from instant_python.configuration.parser.parser import Parser
 from instant_python.dependency_manager.dependency_manager_factory import DependencyManagerFactory
 from instant_python.git.git_configurer import GitConfigurer
 from instant_python.project_creator.file_system import FileSystem
+from instant_python.render.custom_project_renderer import CustomProjectRenderer
 from instant_python.render.jinja_environment import JinjaEnvironment
 from instant_python.render.jinja_project_renderer import JinjaProjectRenderer
 
@@ -13,15 +14,21 @@ app = typer.Typer()
 @app.command("init", help="Create a new project")
 def create_new_project(
     config_file: str = typer.Option(..., "--config", "-c", help="Path to yml configuration file"),
+    template: str | None = typer.Option(None, "--template", "-t", help="Path to custom template file"),
 ) -> None:
     configuration = Parser.parse(config_file_path=config_file)
     environment = JinjaEnvironment(package_name="instant_python", template_directory="new_templates")
 
-    project_renderer = JinjaProjectRenderer(jinja_environment=environment)
-    project_structure = project_renderer.render_project_structure(
-        context_config=configuration,
-        template_base_dir="project_structure",
-    )
+    if template:
+        project_renderer = CustomProjectRenderer(template_path=template)
+        project_structure = project_renderer.render_project_structure()
+    else:
+        project_renderer = JinjaProjectRenderer(jinja_environment=environment)
+        project_structure = project_renderer.render_project_structure(
+            context_config=configuration,
+            template_base_dir="project_structure",
+        )
+
     file_system = FileSystem(project_structure=project_structure)
     file_system.write_on_disk(
         file_renderer=environment,
