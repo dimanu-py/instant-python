b2bdc0ad13ec880bc8cb31f79cbddfd3cd543b1c
dimanu-py <diegomtz126@gmail.com>
2025-06-24 17:23:09 +0200

    ✨ feat(cli): include new "init" command in general application

--- instant_python/cli.py
+++ instant_python/cli.py
@@ -1,7 +1,7 @@
 from rich.console import Console
 from rich.panel import Panel
 
-from instant_python.commands import folder_cli, project_cli
+from instant_python.commands import folder_cli, project_cli, project
 from instant_python.errors.application_error import ApplicationError
 from instant_python.instant_python_typer import InstantPythonTyper
 
@@ -14,6 +14,7 @@ app.add_typer(
     help="Generate only the folder structure for a new project",
 )
 app.add_typer(project_cli.app, name="project", help="Generate a full project ready to be used")
+app.add_typer(project.app)
 
 
 @app.error_handler(ApplicationError)
