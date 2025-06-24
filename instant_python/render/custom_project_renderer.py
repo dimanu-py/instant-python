177715cdc29f5a98dc886f5adf2f832a9252a3dd
dimanu-py <diegomtz126@gmail.com>
2025-06-24 17:19:49 +0200

    🐛 fix(renderer): manually include pyproject.toml boilerplate file when making a project with custom template to be able to create virtual environment

--- instant_python/render/custom_project_renderer.py
+++ instant_python/render/custom_project_renderer.py
@@ -13,4 +13,6 @@ class CustomProjectRenderer:
         if not self._template_path.is_file():
             raise TemplateFileNotFoundError(self._template_path)
         with open(self._template_path, "r", encoding="utf-8") as f:
-            return yaml.safe_load(f)
+            project_structure = yaml.safe_load(f)
+            project_structure.append({"name": "pyproject", "type": "boilerplate_file", "extension": ".toml"})
+            return project_structure
