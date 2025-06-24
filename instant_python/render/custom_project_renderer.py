2d9c7dd73d62c9d1b7343b4e450f03aceccd58cf
dimanu-py <diegomtz126@gmail.com>
2025-06-24 16:37:01 +0200

    ✨ feat(renderer): implement CustomProjectRenderer

---
+++ instant_python/render/custom_project_renderer.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+import yaml
+
+from instant_python.errors.template_file_not_found_error import TemplateFileNotFoundError
+
+
+class CustomProjectRenderer:
+    def __init__(self, template_path: str) -> None:
+        self._template_path = Path(template_path).expanduser().resolve()
+
+    def render_project_structure(self) -> dict[str, str]:
+        if not self._template_path.is_file():
+            raise TemplateFileNotFoundError(self._template_path)
+        with open(self._template_path, "r", encoding="utf-8") as f:
+            return yaml.safe_load(f)
