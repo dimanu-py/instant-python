0cd041636e333a6a56dbbb819ca9802c79764d1c
dimanu-py <diegomtz126@gmail.com>
2025-06-26 20:54:29 +0200

    ♻️ refactor(dependency-manager): move UnknownDependencyManagerError to the dependency manager module

--- instant_python/dependency_manager/dependency_manager_factory.py
+++ instant_python/dependency_manager/dependency_manager_factory.py
@@ -1,7 +1,7 @@
 from instant_python.dependency_manager.dependency_manager import DependencyManager
 from instant_python.dependency_manager.pdm_dependency_manager import PdmDependencyManager
 from instant_python.dependency_manager.uv_dependency_manager import UvDependencyManager
-from instant_python.errors.unknown_dependency_manager_error import UnknownDependencyManagerError
+from instant_python.dependency_manager.unknown_dependency_manager_error import UnknownDependencyManagerError
 from instant_python.shared.supported_managers import SupportedManagers
 
 
