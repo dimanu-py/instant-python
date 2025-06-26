ce9d8823153f4185c67d8fb20515814e6933ab39
dimanu-py <diegomtz126@gmail.com>
2025-06-26 20:15:33 +0200

    ✨ feat(shared): add method to retrieve list of supported managers

--- instant_python/shared/supported_managers.py
+++ instant_python/shared/supported_managers.py
@@ -4,3 +4,7 @@ from enum import Enum
 class SupportedManagers(str, Enum):
     UV = "uv"
     PDM = "pdm"
+
+    @classmethod
+    def get_supported_managers(cls) -> list[str]:
+        return [manager.value for manager in cls]
