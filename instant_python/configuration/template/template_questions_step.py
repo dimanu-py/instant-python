c9c44ac1ac7a451e8cc4b2c949fd6e1e36eec2eb
dimanu-py <diegomtz126@gmail.com>
2025-06-26 18:01:56 +0200

    ✨ feat(configuration): add TemplateStep to manage template selection and built-in features

---
+++ instant_python/configuration/template/template_questions_step.py
@@ -0,0 +1,72 @@
+from instant_python.configuration.question.boolean_question import BooleanQuestion
+from instant_python.configuration.question.choice_question import ChoiceQuestion
+from instant_python.configuration.question.conditional_question import ConditionalQuestion
+from instant_python.configuration.question.free_text_question import FreeTextQuestion
+from instant_python.configuration.question.multiple_choice_question import MultipleChoiceQuestion
+from instant_python.configuration.question.questionary import Questionary
+from instant_python.configuration.template.template_types import TemplateTypes
+
+
+class TemplateStep:
+    def __init__(self, questionary: Questionary) -> None:
+        self._questions = [
+            MultipleChoiceQuestion(
+                key="built_in_features",
+                message="Select the built-in features you want to include",
+                options=[
+                    "value_objects",
+                    "github_actions",
+                    "makefile",
+                    "logger",
+                    "event_bus",
+                    "async_sqlalchemy",
+                    "async_alembic",
+                    "fastapi_application",
+                ],
+                questionary=questionary,
+            ),
+            ConditionalQuestion(
+                base_question=ChoiceQuestion(
+                    key="template",
+                    message="Select a template",
+                    options=[
+                        "domain_driven_design",
+                        "clean_architecture",
+                        "standard_project",
+                        "custom",
+                    ],
+                    questionary=questionary,
+                ),
+                subquestions=ConditionalQuestion(
+                    base_question=BooleanQuestion(
+                        key="specify_bounded_context",
+                        message="Do you want to specify your first bounded context?",
+                        default=True,
+                        questionary=questionary,
+                    ),
+                    subquestions=[
+                        FreeTextQuestion(
+                            key="bounded_context",
+                            message="Enter the bounded context name",
+                            default="backoffice",
+                            questionary=questionary,
+                        ),
+                        FreeTextQuestion(
+                            key="aggregate_name",
+                            message="Enter the aggregate name",
+                            default="user",
+                            questionary=questionary,
+                        ),
+                    ],
+                    condition=True,
+                ),
+                condition=TemplateTypes.DDD,
+            ),
+        ]
+
+    def run(self) -> dict[str, dict[str, str | list[str]]]:
+        answers = {}
+        for question in self._questions:
+            answers.update(question.ask())
+
+        return {"template": answers}
