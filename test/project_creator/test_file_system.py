0c11ee488f7ab06bf747861d81b11db67af84ac9
dimanu-py <diegomtz126@gmail.com>
2025-07-15 20:38:08 +0200

    test(project-creator): add test that verifies fastapi application does not include any reference to logger or migrator if they are not in built in features

--- test/project_creator/test_file_system.py
+++ test/project_creator/test_file_system.py
@@ -24,6 +24,7 @@ class TestFileSystem:
         [
             pytest.param("rendered_project_structure.json", "config.yml", id="base_project_structure"),
             pytest.param("rendered_custom_project_structure.json", "config.yml", id="custom_project_structure"),
+            pytest.param("rendered_project_structure_only_with_fastapi.json", "config_with_only_fastapi.yml", id="only_fastapi_project_structure"),
         ],
     )
     def test_should_create_file_system_in_disk(self, project_structure_file_name: str, config_file: str) -> None:
