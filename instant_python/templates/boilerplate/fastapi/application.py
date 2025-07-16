cfd7d14cd1f515b513774df5f25f2180baee2cb3
dimanu-py <diegomtz126@gmail.com>
2025-07-16 21:51:36 +0200

    feat(templates): separate template files for fastapi error handlers

--- instant_python/templates/boilerplate/fastapi/application.py
+++ instant_python/templates/boilerplate/fastapi/application.py
@@ -1,7 +1,37 @@
 {% set template_domain_import = "shared.domain"|compute_base_path(template.name) %}
 {% set template_infra_import = "shared.infra"|compute_base_path(template.name) %}
-from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI
+{% if "logger" in template.built_in_features %}
+from fastapi.exceptions import RequestValidationError
+{% endif %}
+
+{% if template.name == template_types.STANDARD %}
+{% if "logger" in template.built_in_features %}
+from {{ general.source_name }}.api.handlers.error_handlers import (
+	unexpected_exception_handler,
+	domain_error_handler,
+	validation_error_handler,
+)
+{% else %}
+from {{ general.source_name }}.api.handlers.error_handlers import (
+	unexpected_exception_handler,
+	domain_error_handler,
+)
+{% endif %}
+{% else %}
+{% if "logger" in template.built_in_features %}
+from {{ general.source_name }}.delivery.api.handlers.error_handlers import (
+	unexpected_exception_handler,
+	domain_error_handler,
+	validation_error_handler,
+)
+{% else %}
+from {{ general.source_name }}.delivery.api.handlers.error_handlers import (
+	unexpected_exception_handler,
+	domain_error_handler,
+)
+{% endif %}
+{% endif %}
 
 {% if ["async_alembic"] | is_in(template.built_in_features) %}
 {% if template.name == template_types.STANDARD %}
@@ -10,10 +40,9 @@ from {{ general.source_name }}.api.lifespan import lifespan
 from {{ general.source_name }}.delivery.api.lifespan import lifespan
 {% endif %}
 {% endif %}
-from {{ general.source_name }}.{{ template_infra_import }}.http.error_response import InternalServerError, UnprocessableEntityError, ResourceNotFoundError
 from {{ general.source_name }}.{{ template_domain_import }}.exceptions.domain_error import DomainError
 {% if "logger" in template.built_in_features %}
-from {{ general.source_name }}.{{ template_infra_import }}.logger.fastapi_file_logger import create_api_logger
+from {{ general.source_name }}.{{ template_infra_import }}.logger.file_logger import create_file_logger
 {% if template.name == template_types.STANDARD %}
 from {{ general.source_name }}.api.middleare.fast_api_log_middleware import FastapiLogMiddleware
 {% else %}
@@ -32,42 +61,8 @@ app = FastAPI()
 logger = create_file_logger(name="{{ general.slug }}")
 
 app.add_middleware(FastapiLogMiddleware, logger=logger)
-
-@app.exception_handler(Exception)
-async def unexpected_exception_handler(request: Request, exc: Exception) -> JSONResponse:
-	logger.error(
-		message=f"error - {request.url.path}",
-		details={
-			"error": {
-				"message": str(exc),
-				"type": "unexpected_error",
-			},
-			"method": request.method,
-			"source": request.url.path,
-		},
-	)
-	return InternalServerError().as_json()
-
-
-@app.exception_handler(DomainError)
-async def domain_error_handler(request: Request, exc: DomainError) -> JSONResponse:
-	logger.error(
-		message=f"error - {request.url.path}",
-		details={
-			"error": exc.to_primitives(),
-			"method": request.method,
-			"source": request.url.path,
-		},
-	)
-	return UnprocessableEntityError().as_json()
-{% else %}
-@app.exception_handler(Exception)
-async def unexpected_exception_handler(_: Request, __: Exception) -> JSONResponse:
-	return InternalServerError().as_json()
-
-
-@app.exception_handler(DomainError)
-async def domain_error_handler(_: Request, __: DomainError) -> JSONResponse:
-	return UnprocessableEntityError().as_json()
+app.add_exception_handler(RequestValidationError, validation_error_handler)
 {% endif %}
+app.add_exception_handler(Exception, unexpected_exception_handler)
+app.add_exception_handler(DomainError, domain_error_handler)
 
