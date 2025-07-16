1e15d5d58dbb583fb681df648ddda573ef2c1679
dimanu-py <diegomtz126@gmail.com>
2025-07-16 20:35:51 +0200

    feat(templates): implement new error template as base class for errors and let domain error inherit from it

--- instant_python/templates/boilerplate/exceptions/domain_error.py
+++ instant_python/templates/boilerplate/exceptions/domain_error.py
@@ -1,22 +1,6 @@
-from abc import ABC
+{% set template_domain_import = "shared.domain"|compute_base_path(template.name) %}
+from {{ general.source_name }}.{{ template_domain_import }}.exceptions.error import Error
 
+class DomainError(Error):
+    ...
 
-class DomainError(Exception, ABC):
-    def __init__(self, message: str, error_type: str) -> None:
-        self._message = message
-        self._type = error_type
-        super().__init__(self._message)
-
-    @property
-    def type(self) -> str:
-        return self._type
-
-    @property
-    def message(self) -> str:
-        return self._message
-
-    def to_primitives(self) -> dict[str, str]:
-        return {
-            "type": self.type,
-            "message": self.message,
-        }
