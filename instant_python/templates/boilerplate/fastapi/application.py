021039de56a92bf018dbbbd68d57bc60bbd2126d
dimanu-py <diegomtz126@gmail.com>
2025-07-16 20:01:05 +0200

    feat(templates): modify fastapi main application template with new logger

--- instant_python/templates/boilerplate/fastapi/application.py
+++ instant_python/templates/boilerplate/fastapi/application.py
@@ -12,6 +12,7 @@ from {{ general.source_name }}.delivery.api.lifespan import lifespan
 {% endif %}
 from {{ general.source_name }}.{{ template_infra_import }}.http.error_response import InternalServerError, UnprocessableEntityError, ResourceNotFoundError
 from {{ general.source_name }}.{{ template_domain_import }}.exceptions.domain_error import DomainError
+from {{ general.source_name }}.{{ template_infra_import }}.logger.fastapi_file_logger import create_api_logger
 
 
 {% if ["async_alembic"] | is_in(template.built_in_features) %}
@@ -20,11 +21,46 @@ app = FastAPI(lifespan=lifespan)
 app = FastAPI()
 {% endif %}
 
+{% if "logger" in template.built_in_features % }
+logger = create_file_logger(name="{{ general.slug }}")
+
+app.add_middleware(FastapiLogMiddleware, logger=logger)
+
 @app.exception_handler(Exception)
-async def unexpected_exception_handler(_: Request, exc: Exception) -> JSONResponse:
+async def unexpected_exception_handler(request: Request, exc: Exception) -> JSONResponse:
+	logger.error(
+		message=f"error - {request.url.path}",
+		details={
+			"error": {
+				"message": str(exc),
+				"type": "unexpected_error",
+			},
+			"method": request.method,
+			"source": request.url.path,
+		},
+	)
 	return InternalServerError().as_json()
 
 
 @app.exception_handler(DomainError)
-async def domain_error_handler(_: Request, exc: DomainError) -> JSONResponse:
+async def domain_error_handler(request: Request, exc: DomainError) -> JSONResponse:
+	logger.error(
+		message=f"error - {request.url.path}",
+		details={
+			"error": exc.to_primitives(),
+			"method": request.method,
+			"source": request.url.path,
+		},
+	)
 	return UnprocessableEntityError().as_json()
+{% else %}
+@app.exception_handler(Exception)
+async def unexpected_exception_handler(_: Request, __: Exception) -> JSONResponse:
+	return InternalServerError().as_json()
+
+
+@app.exception_handler(DomainError)
+async def domain_error_handler(_: Request, __: DomainError) -> JSONResponse:
+	return UnprocessableEntityError().as_json()
+{% endif %}
+
