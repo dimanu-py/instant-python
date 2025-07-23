a438eae11ad9e02eda9dffc7337cc037683af4c5
dimanu-py <diegomtz126@gmail.com>
2025-07-23 17:41:08 +0200

    fix(templates): add default templates and object mothers for int value object template

--- instant_python/templates/boilerplate/random_generator.py
+++ instant_python/templates/boilerplate/random_generator.py
@@ -11,3 +11,8 @@ class RandomGenerator:
     @classmethod
     def word(cls) -> str:
         return cls.faker.word()
+
+
+    @classmethod
+    def positive_integer(cls, minimum: int = 0, maximum: int = 100) -> int:
+        return cls.faker.pyint(min_value=minimum, max_value=maximum)
