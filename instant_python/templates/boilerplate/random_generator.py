088016942b3671bc58acc31959e7482c3dff38d7
dimanu-py <diegomtz126@gmail.com>
2025-07-23 17:35:31 +0200

    fix(templates): add default templates and object mothers for string value object template

--- instant_python/templates/boilerplate/random_generator.py
+++ instant_python/templates/boilerplate/random_generator.py
@@ -2,8 +2,12 @@ from faker import Faker
 
 
 class RandomGenerator:
-	faker = Faker()
+    faker = Faker()
 
-	@classmethod
-	def uuid(cls) -> str:
-	    return cls.faker.uuid4()
+    @classmethod
+    def uuid(cls) -> str:
+        return cls.faker.uuid4()
+
+    @classmethod
+    def word(cls) -> str:
+        return cls.faker.word()
