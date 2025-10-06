936b2c61fc69c013e618093bfa62e8599876f2d7
dimanu-py <diegomtz126@gmail.com>
2025-10-06 21:37:51 +0200

    feat(config): implement execute method for configuration generation

--- instant_python/config/application/config_generator.py
+++ instant_python/config/application/config_generator.py
@@ -1,3 +1,8 @@
+from instant_python.config.domain.configuration_schema import ConfigurationSchema
+from instant_python.config.domain.dependency_configuration import DependencyConfiguration
+from instant_python.config.domain.general_configuration import GeneralConfiguration
+from instant_python.config.domain.git_configuration import GitConfiguration
+from instant_python.config.domain.template_configuration import TemplateConfiguration
 from instant_python.config.domain.yaml_writer import YamlWriter
 from instant_python.configuration.question_wizard import QuestionWizard
 
@@ -6,3 +11,13 @@ class ConfigGenerator:
     def __init__(self, question_wizard: QuestionWizard, writer: YamlWriter) -> None:
         self._question_wizard = question_wizard
         self._writer = writer
+
+    def execute(self) -> None:
+        answers = self._question_wizard.run()
+        configuration = ConfigurationSchema(
+            general=GeneralConfiguration(**answers["general"]),
+            dependencies=[DependencyConfiguration(**dependency) for dependency in answers["dependencies"]],
+            template=TemplateConfiguration(**answers["template"]),
+            git=GitConfiguration(**answers["git"]),
+        )
+        self._writer.write(configuration)
