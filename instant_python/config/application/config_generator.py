2a3bea55ae87317580012ab6eb940675c6089c4a
dimanu-py <diegomtz126@gmail.com>
2025-10-07 08:51:14 +0200

    refactor(config): decouple configuration generation from specific schema classes validating answers with parser

--- instant_python/config/application/config_generator.py
+++ instant_python/config/application/config_generator.py
@@ -1,11 +1,6 @@
 from instant_python.config.domain.config_parser import ConfigParser
 from instant_python.config.domain.config_writer import ConfigWriter
-from instant_python.config.domain.configuration_schema import ConfigurationSchema
-from instant_python.config.domain.dependency_configuration import DependencyConfiguration
-from instant_python.config.domain.general_configuration import GeneralConfiguration
-from instant_python.config.domain.git_configuration import GitConfiguration
 from instant_python.config.domain.question_wizard import QuestionWizard
-from instant_python.config.domain.template_configuration import TemplateConfiguration
 
 
 class ConfigGenerator:
@@ -16,10 +11,5 @@ class ConfigGenerator:
 
     def execute(self) -> None:
         answers = self._question_wizard.run()
-        configuration = ConfigurationSchema(
-            general=GeneralConfiguration(**answers["general"]),
-            dependencies=[DependencyConfiguration(**dependency) for dependency in answers["dependencies"]],
-            template=TemplateConfiguration(**answers["template"]),
-            git=GitConfiguration(**answers["git"]),
-        )
+        configuration = self._parser.parse(answers)
         self._writer.write(configuration)
