14ae0e28830a0857767fe0326fe62b30f9e3089d
dimanu-py <diegomtz126@gmail.com>
2025-10-07 09:17:49 +0200

    test(config): add new test case to verify all keys of answers are present

--- test/config/infra/parser/test_parser.py
+++ test/config/infra/parser/test_parser.py
@@ -1,6 +1,10 @@
+from pathlib import Path
+
+import yaml
 from expects import expect, raise_error
 
 from instant_python.config.infra.parser.parser import Parser
+from instant_python.configuration.parser.config_key_not_present import ConfigKeyNotPresent
 from instant_python.configuration.parser.empty_configuration_not_allowed import EmptyConfigurationNotAllowed
 
 
@@ -10,3 +14,14 @@ class TestParser:
         empty_answers = {}
 
         expect(lambda: parser.parse(empty_answers)).to(raise_error(EmptyConfigurationNotAllowed))
+
+    def test_should_raise_error_if_some_section_is_missing(self) -> None:
+        parser = Parser()
+        answers = self._read_fake_answers_from_file("missing_keys_answers")
+
+        expect(lambda: parser.parse(answers)).to(raise_error(ConfigKeyNotPresent))
+
+    @staticmethod
+    def _read_fake_answers_from_file(file_name: str) -> dict[str, dict]:
+        with open(Path(__file__).parent / "resources" / f"{file_name}.yml") as answers:
+            return yaml.safe_load(answers)
