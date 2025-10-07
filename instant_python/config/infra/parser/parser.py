e6ef56c2988e8af8d0a9ffadcc882c643a2e580e
dimanu-py <diegomtz126@gmail.com>
2025-10-07 09:30:48 +0200

    feat(config): implement logic to parse general section of config

--- instant_python/config/infra/parser/parser.py
+++ instant_python/config/infra/parser/parser.py
@@ -1,7 +1,9 @@
 from instant_python.config.domain.config_parser import ConfigParser
 from instant_python.config.domain.configuration_schema import ConfigurationSchema
+from instant_python.config.domain.general_configuration import GeneralConfiguration
 from instant_python.configuration.parser.config_key_not_present import ConfigKeyNotPresent
 from instant_python.configuration.parser.empty_configuration_not_allowed import EmptyConfigurationNotAllowed
+from instant_python.configuration.parser.missing_mandatory_fields import MissingMandatoryFields
 
 
 class Parser(ConfigParser):
@@ -10,13 +12,26 @@ class Parser(ConfigParser):
     def parse(self, content: dict[str, dict]) -> ConfigurationSchema:
         self._ensure_configuration_is_not_empty(content)
         self._ensure_all_required_sections_are_present(content)
+        general_section = self._parse_general_section(content["general"])
 
     def _ensure_all_required_sections_are_present(self, content: dict[str, dict]):
         missing_keys = [key for key in self._REQUIRED_CONFIG_KEYS if key not in content]
         if missing_keys:
             raise ConfigKeyNotPresent(missing_keys, self._REQUIRED_CONFIG_KEYS)
 
+    def _parse_general_section(self, fields: dict[str, str]) -> GeneralConfiguration:
+        try:
+            return GeneralConfiguration(**fields)
+        except TypeError as error:
+            self._ensure_error_is_for_missing_fields(error)
+            raise MissingMandatoryFields(error.args[0], "general") from error
+
     @staticmethod
     def _ensure_configuration_is_not_empty(content: dict[str, dict]):
         if not content:
             raise EmptyConfigurationNotAllowed
+
+    @staticmethod
+    def _ensure_error_is_for_missing_fields(error: TypeError) -> None:
+        if ".__init__() missing" not in str(error):
+            raise error
