b6fec2a4c2d7b9773b7bca1707acf6193e0dd986
dimanu-py <diegomtz126@gmail.com>
2025-10-07 09:48:37 +0200

    feat(config): add parsing logic for dependencies section in configuration

--- instant_python/config/infra/parser/parser.py
+++ instant_python/config/infra/parser/parser.py
@@ -1,5 +1,8 @@
+from typing import Union
+
 from instant_python.config.domain.config_parser import ConfigParser
 from instant_python.config.domain.configuration_schema import ConfigurationSchema
+from instant_python.config.domain.dependency_configuration import DependencyConfiguration
 from instant_python.config.domain.general_configuration import GeneralConfiguration
 from instant_python.configuration.parser.config_key_not_present import ConfigKeyNotPresent
 from instant_python.configuration.parser.empty_configuration_not_allowed import EmptyConfigurationNotAllowed
@@ -13,11 +16,7 @@ class Parser(ConfigParser):
         self._ensure_configuration_is_not_empty(content)
         self._ensure_all_required_sections_are_present(content)
         general_section = self._parse_general_section(content["general"])
-
-    def _ensure_all_required_sections_are_present(self, content: dict[str, dict]):
-        missing_keys = [key for key in self._REQUIRED_CONFIG_KEYS if key not in content]
-        if missing_keys:
-            raise ConfigKeyNotPresent(missing_keys, self._REQUIRED_CONFIG_KEYS)
+        dependencies_section = self._parse_dependencies_section(content["dependencies"])
 
     def _parse_general_section(self, fields: dict[str, str]) -> GeneralConfiguration:
         try:
@@ -25,6 +24,31 @@ class Parser(ConfigParser):
         except TypeError as error:
             self._ensure_error_is_for_missing_fields(error)
             raise MissingMandatoryFields(error.args[0], "general") from error
+
+    def _parse_dependencies_section(
+        self,
+        fields: list[dict[str, Union[str, bool]]],
+    ) -> list[DependencyConfiguration]:
+        dependencies = []
+
+        if not fields:
+            return dependencies
+
+        for dependency_fields in fields:
+            try:
+                dependency = DependencyConfiguration(**dependency_fields)
+            except TypeError as error:
+                self._ensure_error_is_for_missing_fields(error)
+                raise MissingMandatoryFields(error.args[0], "dependencies") from error
+
+            dependencies.append(dependency)
+
+        return dependencies
+
+    def _ensure_all_required_sections_are_present(self, content: dict[str, dict]):
+        missing_keys = [key for key in self._REQUIRED_CONFIG_KEYS if key not in content]
+        if missing_keys:
+            raise ConfigKeyNotPresent(missing_keys, self._REQUIRED_CONFIG_KEYS)
 
     @staticmethod
     def _ensure_configuration_is_not_empty(content: dict[str, dict]):
