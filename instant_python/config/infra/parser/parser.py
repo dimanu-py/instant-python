b3f51e4a5dd483d4a2f3fa58c2e4b3b035dec1ca
dimanu-py <diegomtz126@gmail.com>
2025-10-07 09:13:32 +0200

    feat(config): raise EmptyConfigurationNotAllowed for empty content in parse method

--- instant_python/config/infra/parser/parser.py
+++ instant_python/config/infra/parser/parser.py
@@ -1,7 +1,8 @@
 from instant_python.config.domain.config_parser import ConfigParser
 from instant_python.config.domain.configuration_schema import ConfigurationSchema
+from instant_python.configuration.parser.empty_configuration_not_allowed import EmptyConfigurationNotAllowed
 
 
 class Parser(ConfigParser):
     def parse(self, content: dict[str, dict]) -> ConfigurationSchema:
-        raise NotImplementedError
+        raise EmptyConfigurationNotAllowed
