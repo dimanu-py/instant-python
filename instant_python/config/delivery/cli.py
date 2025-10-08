8785304b744f9dba777e2518014e4a40146753c0
dimanu-py <diegomtz126@gmail.com>
2025-10-08 17:54:42 +0200

    refactor(config): instantiate question wizard steps inside directly coupling them

--- instant_python/config/delivery/cli.py
+++ instant_python/config/delivery/cli.py
@@ -4,11 +4,6 @@ from instant_python.config.application.config_generator import ConfigGenerator
 from instant_python.config.infra.parser.parser import Parser
 from instant_python.config.infra.question_wizard.step.questionary import Questionary
 from instant_python.config.infra.question_wizard.questionary_console_wizard import QuestionaryConsoleWizard
-from instant_python.config.infra.question_wizard.step.dependencies_step import DependenciesStep
-from instant_python.config.infra.question_wizard.step.general_step import GeneralStep
-from instant_python.config.infra.question_wizard.step.git_step import GitStep
-from instant_python.config.infra.question_wizard.step.steps import Steps
-from instant_python.config.infra.question_wizard.step.template_step import TemplateStep
 from instant_python.config.infra.writer.yaml_config_writer import YamlConfigWriter
 
 app = typer.Typer()
@@ -16,20 +11,8 @@ app = typer.Typer()
 
 @app.command("config", help="Generate the configuration file for a new project")
 def generate_ipy_configuration_file() -> None:
-    questionary = Questionary()
-    question_wizard = QuestionaryConsoleWizard(
-        steps=(
-            Steps(
-                GeneralStep(questionary=questionary),
-                TemplateStep(questionary=questionary),
-                GitStep(questionary=questionary),
-                DependenciesStep(questionary=questionary),
-            )
-        )
-    )
-
     config_generator = ConfigGenerator(
-        question_wizard=question_wizard,
+        question_wizard=QuestionaryConsoleWizard(questionary=(Questionary())),
         writer=YamlConfigWriter(),
         parser=Parser(),
     )
--- instant_python/config/infra/question_wizard/questionary_console_wizard.py
+++ instant_python/config/infra/question_wizard/questionary_console_wizard.py
@@ -1,10 +1,20 @@
 from instant_python.config.domain.question_wizard import QuestionWizard
+from instant_python.config.infra.question_wizard.step.dependencies_step import DependenciesStep
+from instant_python.config.infra.question_wizard.step.general_step import GeneralStep
+from instant_python.config.infra.question_wizard.step.git_step import GitStep
+from instant_python.config.infra.question_wizard.step.questionary import Questionary
 from instant_python.config.infra.question_wizard.step.steps import Steps
+from instant_python.config.infra.question_wizard.step.template_step import TemplateStep
 
 
 class QuestionaryConsoleWizard(QuestionWizard):
-    def __init__(self, steps: Steps) -> None:
-        self._steps = steps
+    def __init__(self, questionary: Questionary) -> None:
+        self._steps = Steps(
+            GeneralStep(questionary=questionary),
+            TemplateStep(questionary=questionary),
+            GitStep(questionary=questionary),
+            DependenciesStep(questionary=questionary),
+        )
         self._answers = {}
 
     def run(self) -> dict:
