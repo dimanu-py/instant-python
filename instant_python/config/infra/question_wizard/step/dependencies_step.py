33f0bd6d95d7aa0d9c87a74c2a5e0a638b1541a1
dimanu-py <diegomtz126@gmail.com>
2025-10-08 17:01:57 +0200

    refactor(config): streamline dependency installation questions in CLI

--- instant_python/config/infra/question_wizard/step/dependencies_step.py
+++ instant_python/config/infra/question_wizard/step/dependencies_step.py
@@ -14,56 +14,44 @@ class DependenciesStep(Step):
     def run(self) -> dict[str, list[dict[str, Union[str, bool]]]]:
         dependencies = []
         while True:
-            user_wants_to_install_dependencies = BooleanQuestion(
-                key="keep_asking",
+            user_wants_to_install_dependencies = self._questionary.boolean_question(
                 message="Do you want to install dependencies?",
-                default=False,
-                questionary=self._questionary,
-            ).ask()["keep_asking"]
+            )
 
             if not user_wants_to_install_dependencies:
                 break
 
-            dependency_name = FreeTextQuestion(
-                key="name",
+            name = self._questionary.free_text_question(
                 message="Enter the name of the dependency you want to install",
-                questionary=self._questionary,
-            ).ask()
+            )
 
-            if not dependency_name["name"]:
+            if not name:
                 print("Dependency name cannot be empty. Let's try again.")
                 continue
 
-            version = FreeTextQuestion(
-                key="version",
-                message="Enter the version of the dependency",
+            version = self._questionary.free_text_question(
+                message="Enter the version of the dependency you want to install",
                 default="latest",
-                questionary=self._questionary,
-            ).ask()
+            )
 
-            is_dev_and_belongs_to_group = ConditionalQuestion(
-                base_question=BooleanQuestion(
-                    key="is_dev",
-                    message=f"Do you want to install {dependency_name['name']} as a dev dependency?",
-                    default=False,
-                    questionary=self._questionary,
-                ),
-                subquestions=[
-                    FreeTextQuestion(
-                        key="group",
-                        message="Specify the name of the group where to install the dependency (leave empty if not applicable)",
-                        default="",
-                        questionary=self._questionary,
-                    )
-                ],
-                condition=True,
-            ).ask()
+            is_for_development = self._questionary.boolean_question(
+                message=f"Do you want to install {name} as a dev dependency?",
+                default=False,
+            )
+
+            group_name = ""
+            if is_for_development:
+                group_name = self._questionary.free_text_question(
+                    message="Specify the name of the group where to install the dependency (leave empty if not applicable)",
+                    default="",
+                )
 
             dependencies.append(
                 {
-                    **dependency_name,
-                    **version,
-                    **is_dev_and_belongs_to_group,
+                    "name": name,
+                    "version": version,
+                    "is_dev": is_for_development,
+                    "group": group_name,
                 }
             )
 
