530fe7e47ed7f9fb81077c4efdbe31c0967e339c
dimanu-py <diegomtz126@gmail.com>
2025-10-08 16:11:10 +0200

    refactor(config): modify cli application to use new config generator use case

--- instant_python/config/delivery/cli.py
+++ instant_python/config/delivery/cli.py
@@ -1,28 +1,36 @@
 import typer
 
-from instant_python.configuration.parser.parser import Parser
-from instant_python.config.infra.question_wizard.question import Questionary
+from instant_python.config.application.config_generator import ConfigGenerator
+from instant_python.config.infra.parser.parser import Parser
+from instant_python.config.infra.question_wizard.question.questionary import Questionary
 from instant_python.config.infra.question_wizard.questionary_console_wizard import QuestionaryConsoleWizard
-from instant_python.config.infra.question_wizard.step import DependenciesStep
-from instant_python.config.infra.question_wizard.step import GeneralStep
-from instant_python.config.infra.question_wizard.step import GitStep
-from instant_python.config.infra.question_wizard.step import Steps
-from instant_python.config.infra.question_wizard.step import TemplateStep
+from instant_python.config.infra.question_wizard.step.dependencies_step import DependenciesStep
+from instant_python.config.infra.question_wizard.step.general_step import GeneralStep
+from instant_python.config.infra.question_wizard.step.git_step import GitStep
+from instant_python.config.infra.question_wizard.step.steps import Steps
+from instant_python.config.infra.question_wizard.step.template_step import TemplateStep
+from instant_python.config.infra.writer.yaml_config_writer import YamlConfigWriter
 
 app = typer.Typer()
 
 
 @app.command("config", help="Generate the configuration file for a new project")
-def create_new_project() -> None:
+def generate_ipy_configuration_file() -> None:
     questionary = Questionary()
-    steps = Steps(
-        GeneralStep(questionary=questionary),
-        TemplateStep(questionary=questionary),
-        GitStep(questionary=questionary),
-        DependenciesStep(questionary=questionary),
+    question_wizard = QuestionaryConsoleWizard(
+        steps=(
+            Steps(
+                GeneralStep(questionary=questionary),
+                TemplateStep(questionary=questionary),
+                GitStep(questionary=questionary),
+                DependenciesStep(questionary=questionary),
+            )
+        )
     )
 
-    question_wizard = QuestionaryConsoleWizard(steps=steps)
-    configuration = question_wizard.run()
-    validated_configuration = Parser.parse_from_answers(configuration)
-    validated_configuration.save_on_current_directory()
+    config_generator = ConfigGenerator(
+        question_wizard=question_wizard,
+        writer=YamlConfigWriter(),
+        parser=Parser(),
+    )
+    config_generator.execute()
