a00937100e8cef46036dce1dc16db18f20602a03
dimanu-py <diegomtz126@gmail.com>
2025-10-24 08:58:20 +0200

    feat(initialize): implement execute method to return parsed configuration schema from config file

--- instant_python/initialize/application/config_reader.py
+++ instant_python/initialize/application/config_reader.py
@@ -1,4 +1,5 @@
 from instant_python.config.domain.config_parser import ConfigParser
+from instant_python.config.domain.configuration_schema import ConfigurationSchema
 from instant_python.initialize.domain.configuration_repository import ConfigurationRepository
 
 
@@ -7,5 +8,7 @@ class ConfigReader:
         self._repository = repository
         self._parser = parser
 
-    def execute(self, config_file_path: str) -> None:
-        raise NotImplementedError()
+    def execute(self, config_file_path: str) -> ConfigurationSchema:
+        raw_config = self._repository.read_from_file(config_file_path)
+        configuration = self._parser.parse(raw_config)
+        return configuration
