00fb79f5ec282525433b5d0e6abb4880cac23b33
dimanu-py <diegomtz126@gmail.com>
2025-10-24 10:24:09 +0200

    feat(initialize): handle FileNotFoundError in read_from_file method

--- instant_python/initialize/infra/configuration_repository.py
+++ instant_python/initialize/infra/configuration_repository.py
@@ -2,10 +2,14 @@ from pathlib import Path
 
 import yaml
 
+from instant_python.configuration.parser.configuration_file_not_found import ConfigurationFileNotFound
 from instant_python.initialize.domain.configuration_repository import ConfigurationRepository
 
 
 class YamlConfigurationRepository(ConfigurationRepository):
     def read_from_file(self, path: str) -> dict:
-        with Path(path).open("r") as file:
-            return yaml.safe_load(file)
+        try:
+            with Path(path).open("r") as file:
+                return yaml.safe_load(file)
+        except FileNotFoundError:
+            raise ConfigurationFileNotFound(path)
