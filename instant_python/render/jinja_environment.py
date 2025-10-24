caf805b1f9c38782e011d237cc131798c6c9b565
dimanu-py <diegomtz126@gmail.com>
2025-10-24 14:02:11 +0200

    refactor(render): encapsulate filter addition in _add_filter method

--- instant_python/render/jinja_environment.py
+++ instant_python/render/jinja_environment.py
@@ -1,3 +1,5 @@
+from collections.abc import Callable
+
 from jinja2 import Environment, PackageLoader
 
 from instant_python.render.jinja_custom_filters import is_in, compute_base_path, has_dependency
@@ -11,9 +13,9 @@ class JinjaEnvironment:
             lstrip_blocks=True,
             autoescape=True,
         )
-        self._env.filters["is_in"] = is_in
-        self._env.filters["compute_base_path"] = compute_base_path
-        self._env.filters["has_dependency"] = has_dependency
+        self._add_filter("is_in", is_in)
+        self._add_filter("compute_base_path", compute_base_path)
+        self._add_filter("has_dependency", has_dependency)
 
     def render_template(self, name: str, context: dict[str, str] = None) -> str:
         """Renders a template with the given context.
@@ -27,3 +29,6 @@ class JinjaEnvironment:
         """
         template = self._env.get_template(name)
         return template.render(**(context or {}))
+
+    def _add_filter(self, name: str, filter_: Callable) -> None:
+        self._env.filters[name] = filter_
