5630929f3a5615e0f93d6d688e01d07cc31bafbd
dimanu-py <diegomtz126@gmail.com>
2025-10-24 10:17:33 +0200

    feat(initialize): implement read_from_file method to load YAML configuration

--- instant_python/initialize/infra/configuration_repository.py
+++ instant_python/initialize/infra/configuration_repository.py
@@ -1,6 +1,11 @@
+from pathlib import Path
+
+import yaml
+
 from instant_python.initialize.domain.configuration_repository import ConfigurationRepository
 
 
 class YamlConfigurationRepository(ConfigurationRepository):
     def read_from_file(self, path: str) -> dict:
-        pass
+        with Path(path).open("r") as file:
+            return yaml.safe_load(file)
