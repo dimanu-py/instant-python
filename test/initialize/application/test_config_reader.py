b09ca55e40e63b9610e699c6c800e0aa31270d1c
dimanu-py <diegomtz126@gmail.com>
2025-10-24 09:03:40 +0200

    test(initialize): set preconditions and expected calls for config reader injected mocks

--- test/initialize/application/test_config_reader.py
+++ test/initialize/application/test_config_reader.py
@@ -1,10 +1,11 @@
-from doublex import Mock
+from doublex import Mock, expect_call
 from doublex_expects import have_been_satisfied
 from expects import expect, be_none
 
 from instant_python.config.domain.config_parser import ConfigParser
 from instant_python.initialize.application.config_reader import ConfigReader
 from instant_python.initialize.domain.configuration_repository import ConfigurationRepository
+from test.config.domain.mothers.configuration_schema_mother import ConfigurationSchemaMother
 
 
 class TestConfigReader:
@@ -16,6 +17,10 @@ class TestConfigReader:
             parser=configuration_parser,
         )
         config_file_path = "path/to/config/file.yml"
+        config = ConfigurationSchemaMother.any()
+
+        expect_call(configuration_repository).read_from_file(config_file_path).returns(config.to_primitives())
+        expect_call(configuration_parser).parse(config.to_primitives()).returns(config)
 
         parsed_config = config_reader.execute(config_file_path)
 
