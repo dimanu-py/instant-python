25152ee64fb3f70feec6c34a6b782c1b9ec3b35d
dimanu-py <diegomtz126@gmail.com>
2025-10-24 10:21:43 +0200

    test(initialize): add test for error when reading non-existent config file

--- test/initialize/infra/test_configuration_repository.py
+++ test/initialize/infra/test_configuration_repository.py
@@ -2,8 +2,9 @@ import json
 from pathlib import Path
 
 from approvaltests import verify
-from expects import expect, be_none
+from expects import expect, be_none, raise_error
 
+from instant_python.configuration.parser.configuration_file_not_found import ConfigurationFileNotFound
 from instant_python.initialize.infra.configuration_repository import YamlConfigurationRepository
 
 
@@ -16,3 +17,9 @@ class TestConfigurationRepository:
 
         expect(raw_config).to_not(be_none)
         verify(json.dumps(raw_config, indent=2))
+
+    def test_should_raise_error_when_file_to_read_does_not_exist(self) -> None:
+        repository = YamlConfigurationRepository()
+        config_path = "non/existing/path/config.yml"
+
+        expect(lambda: repository.read_from_file(config_path)).to(raise_error(ConfigurationFileNotFound))
