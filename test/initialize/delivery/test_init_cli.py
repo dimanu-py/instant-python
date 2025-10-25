62e1f7abf174442850f2e807de8c43304152b02c
dimanu-py <diegomtz126@gmail.com>
2025-10-26 01:02:13 +0200

    test(initialize): separate acceptance test in three different groups to avoid test to take extra time and make unnecessary combinations

--- test/initialize/delivery/test_init_cli.py
+++ test/initialize/delivery/test_init_cli.py
@@ -18,44 +18,79 @@ class TestInitCli:
     def setup_method(self) -> None:
         self._runner = CliRunner()
 
-    def test_initializes_project_structure(self) -> None:
+    def test_initializes_project_with_general_section_combinations(self) -> None:
         dependency_managers = SupportedManagers.get_supported_managers()
         licenses = SupportedLicenses.get_supported_licenses()
         python_versions = SupportedPythonVersions.get_supported_versions()
-        create_git_repository = [True, False]
-        templates = SupportedTemplates.get_supported_templates()
-        built_in_feature = SupportedBuiltInFeatures.get_supported_built_in_features()
 
         verify_all_combinations(
-            self._run_cli_with_config,
+            self._run_cli_with_general_config,
             [
                 dependency_managers,
                 licenses,
                 python_versions,
-                create_git_repository,
-                templates,
-                built_in_feature,
             ],
         )
 
-    def _run_cli_with_config(
+    def test_initializes_project_with_git_section_combinations(self) -> None:
+        create_git_repository = [True, False]
+
+        verify_all_combinations(
+            self._run_cli_with_git_config,
+            [
+                create_git_repository,
+            ],
+        )
+
+    def test_initializes_project_with_template_section_combinations(self) -> None:
+        templates = SupportedTemplates.get_supported_templates()
+        built_in_features = SupportedBuiltInFeatures.get_supported_built_in_features()
+
+        verify_all_combinations(
+            self._run_cli_with_template_config,
+            [
+                templates,
+                built_in_features,
+            ],
+        )
+
+    def _run_cli_with_general_config(
         self,
         dependency_manager: str,
         license_type: str,
         python_version: str,
-        initialize_git: bool,
-        template: str,
-        built_in_feature: str,
     ) -> dict:
-        config = self._create_config_with_parameters(
+        config = self._create_general_config(
             dependency_manager=dependency_manager,
             license_type=license_type,
             python_version=python_version,
+        )
+
+        return self._run_cli_with_config(config)
+
+    def _run_cli_with_git_config(
+        self,
+        initialize_git: bool,
+    ) -> dict:
+        config = self._create_git_config(
             initialize_git=initialize_git,
+        )
+
+        return self._run_cli_with_config(config)
+
+    def _run_cli_with_template_config(
+        self,
+        template: str,
+        built_in_feature: str,
+    ) -> dict:
+        config = self._create_template_config(
             template=template,
             built_in_feature=built_in_feature,
         )
 
+        return self._run_cli_with_config(config)
+
+    def _run_cli_with_config(self, config: dict) -> dict:
         with tempfile.NamedTemporaryFile(mode="w", suffix=".yml", delete=False) as config_file:
             yaml.dump(config, config_file)
             config_file_path = config_file.name
@@ -66,24 +101,13 @@ class TestInitCli:
         return {
             "exit_code": result.exit_code,
             "errors": result.exception,
-            "config": {
-                "dependency_manager": dependency_manager,
-                "license": license_type,
-                "python_version": python_version,
-                "initialize_git": initialize_git,
-                "template": template,
-                "built_in_feature": built_in_feature,
-            },
         }
 
-    def _create_config_with_parameters(
+    def _create_general_config(
         self,
         dependency_manager: str,
         license_type: str,
         python_version: str,
-        initialize_git: bool,
-        template: str,
-        built_in_feature: str,
     ) -> dict:
         config = json.loads(json.dumps(self._read_base_config()))
 
@@ -91,7 +115,24 @@ class TestInitCli:
         config["general"]["license"] = license_type
         config["general"]["python_version"] = python_version
 
+        return config
+
+    def _create_git_config(
+        self,
+        initialize_git: bool,
+    ) -> dict:
+        config = json.loads(json.dumps(self._read_base_config()))
+
         config["git"]["initialize"] = initialize_git
+
+        return config
+
+    def _create_template_config(
+        self,
+        template: str,
+        built_in_feature: str,
+    ) -> dict:
+        config = json.loads(json.dumps(self._read_base_config()))
 
         config["template"]["name"] = template
         config["template"]["built_in_features"] = [built_in_feature]
