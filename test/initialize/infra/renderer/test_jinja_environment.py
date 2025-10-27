446fd2ff843108c596df6586d7c37e18a42b7ead
dimanu-py <diegomtz126@gmail.com>
2025-10-27 21:38:34 +0100

    test(initialize): verify jinja environment renders template from custom path when is found

--- test/initialize/infra/renderer/test_jinja_environment.py
+++ test/initialize/infra/renderer/test_jinja_environment.py
@@ -1,4 +1,6 @@
-from expects import be_none, expect, have_keys
+from pathlib import Path
+
+from expects import be_none, expect, have_keys, equal
 
 from instant_python.initialize.infra.renderer.jinja_environment import JinjaEnvironment
 
@@ -15,3 +17,11 @@ class TestJinjaEnvironment:
         jinja_environment.add_filter("custom_filter", lambda x: x)
 
         expect(jinja_environment._env.filters).to(have_keys("custom_filter"))
+
+    def test_should_render_template_from_user_templates_folder_when_template_is_found(self) -> None:
+        template_path = str(Path(__file__).parent / "resources")
+        jinja_environment = JinjaEnvironment(template_path)
+
+        rendered_content = jinja_environment.render_template("hello_world.j2", {"name": "World"})
+
+        expect(rendered_content).to(equal("Hello World!"))
