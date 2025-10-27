31ff4a50d1a6b8dff97813ccc4fe99db4376301b
dimanu-py <diegomtz126@gmail.com>
2025-10-27 21:17:30 +0100

    feat(initialize): add test to verify jinja environment loads custom filters

--- test/initialize/infra/renderer/test_jinja_environment.py
+++ test/initialize/infra/renderer/test_jinja_environment.py
@@ -1,4 +1,4 @@
-from expects import be_none, expect
+from expects import be_none, expect, have_keys
 
 from instant_python.initialize.infra.renderer.jinja_environment import JinjaEnvironment
 
@@ -8,3 +8,10 @@ class TestJinjaEnvironment:
         jinja_environment = JinjaEnvironment()
 
         expect(jinja_environment._env).not_to(be_none)
+
+    def test_should_register_custom_filters(self) -> None:
+        jinja_environment = JinjaEnvironment()
+
+        jinja_environment.add_filter("custom_filter", lambda x: x)
+
+        expect(jinja_environment._env.filters).to(have_keys("custom_filter"))
