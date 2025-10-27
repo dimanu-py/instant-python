c0f7ec0d39f721d999cd4a9330c3fab7c32192c9
dimanu-py <diegomtz126@gmail.com>
2025-10-27 21:09:00 +0100

    feat(initialize): instantiate jinja 2 environment inside wrapper class

--- instant_python/initialize/infra/renderer/jinja_environment.py
+++ instant_python/initialize/infra/renderer/jinja_environment.py
@@ -1,3 +1,10 @@
+from jinja2 import Environment
+
+
 class JinjaEnvironment:
     def __init__(self) -> None:
-        ...
+        self._env = Environment(
+            trim_blocks=True,
+            lstrip_blocks=True,
+            autoescape=True,
+        )
