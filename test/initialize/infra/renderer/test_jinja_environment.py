5d1b22221d3a98f3fb06aa3151370a1694d3b37a
dimanu-py <diegomtz126@gmail.com>
2025-10-28 11:04:37 +0100

    test(initialize): add unit test to verify default templates folder is searched when template is not found in user custom path

--- test/initialize/infra/renderer/test_jinja_environment.py
+++ test/initialize/infra/renderer/test_jinja_environment.py
@@ -25,3 +25,10 @@ class TestJinjaEnvironment:
         rendered_content = jinja_environment.render_template("hello_world.j2", {"name": "World"})
 
         expect(rendered_content).to(equal("Hello World!"))
+
+    def test_should_render_template_from_default_templates_folder_when_custom_template_is_not_found(self) -> None:
+        jinja_environment = JinjaEnvironment(".")
+
+        rendered_content = jinja_environment.render_template("makefile")
+
+        expect(rendered_content).to_not(be_none)
