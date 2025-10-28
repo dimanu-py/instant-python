c73e2e38e75d839a1c1ae8a63798db9630d05fd9
dimanu-py <diegomtz126@gmail.com>
2025-10-28 17:57:00 +0100

    refactor(initialize): clean up jinja environment tests extracting common object instantiation

--- test/initialize/infra/renderer/test_jinja_environment.py
+++ test/initialize/infra/renderer/test_jinja_environment.py
@@ -7,33 +7,26 @@ from instant_python.initialize.infra.renderer.jinja_environment import JinjaEnvi
 
 
 class TestJinjaEnvironment:
-    def test_should_initialize_environment(self) -> None:
-        jinja_environment = JinjaEnvironment(".")
+    def setup_method(self) -> None:
+        self._jinja_environment = JinjaEnvironment(user_template_path=str(Path(__file__).parent / "resources"))
 
-        expect(jinja_environment._env).not_to(be_none)
+    def test_should_initialize_environment(self) -> None:
+        expect(self._jinja_environment._env).not_to(be_none)
 
     def test_should_register_custom_filters(self) -> None:
-        jinja_environment = JinjaEnvironment(".")
+        self._jinja_environment.add_filter("custom_filter", lambda x: x)
 
-        jinja_environment.add_filter("custom_filter", lambda x: x)
-
-        expect(jinja_environment._env.filters).to(have_keys("custom_filter"))
+        expect(self._jinja_environment._env.filters).to(have_keys("custom_filter"))
 
     def test_should_render_template_from_user_templates_folder_when_template_is_found(self) -> None:
-        template_path = str(Path(__file__).parent / "resources")
-        jinja_environment = JinjaEnvironment(template_path)
-
-        rendered_content = jinja_environment.render_template("hello_world.j2", {"name": "World"})
+        rendered_content = self._jinja_environment.render_template("hello_world.j2", {"name": "World"})
 
         expect(rendered_content).to(equal("Hello World!"))
 
     def test_should_render_template_from_default_templates_folder_when_custom_template_is_not_found(self) -> None:
-        jinja_environment = JinjaEnvironment(".")
-
+        rendered_content = self._jinja_environment.render_template(".gitignore")
 
         expect(rendered_content).to_not(be_none)
 
     def test_should_raise_error_when_template_is_not_found_anywhere(self) -> None:
-        jinja_environment = JinjaEnvironment(".")
-
-        expect(lambda: jinja_environment.render_template("non_existing_template.j2")).to(raise_error(TemplateNotFound))
+        expect(lambda: self._jinja_environment.render_template("non_existing_template.j2")).to(raise_error(TemplateNotFound))
