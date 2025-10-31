f61063922d0e64c3fbb19832d6e663c5b15adbad
dimanu-py <diegomtz126@gmail.com>
2025-10-31 23:47:56 +0100

    refactor(initialize): extract semantic method to express operation of rendering the project structure using jinja

--- instant_python/initialize/infra/renderer/jinja_project_renderer.py
+++ instant_python/initialize/infra/renderer/jinja_project_renderer.py
@@ -13,6 +13,9 @@ class JinjaProjectRenderer(ProjectRenderer):
 
     def render(self, context_config: ConfigSchema) -> list[dict]:
         template_name = self._get_project_main_structure_template(context_config.template_type)
+        return self._render_project_structure_with_jinja(context_config, template_name)
+
+    def _render_project_structure_with_jinja(self, context_config: ConfigSchema, template_name: str) -> list[dict]:
         raw_project_structure = self._env.render_template(
             name=template_name, context=context_config.to_primitives()
         )
