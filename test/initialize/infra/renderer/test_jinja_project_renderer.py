ed877da26d5d389634a8efd3bd7d4ebfebac569d
dimanu-py <diegomtz126@gmail.com>
2025-10-31 23:23:35 +0100

    test(initialize): modify project renderer test to use first level resources folder

--- test/initialize/infra/renderer/test_jinja_project_renderer.py
+++ test/initialize/infra/renderer/test_jinja_project_renderer.py
@@ -3,12 +3,13 @@ from expects import be_none, expect, be_empty
 from instant_python.initialize.infra.renderer.jinja_environment import JinjaEnvironment
 from instant_python.initialize.infra.renderer.jinja_project_renderer import JinjaProjectRenderer
 from test.config.domain.mothers.config_schema_mother import ConfigSchemaMother
+from test.initialize.utils import test_resources_path
 
 
 class TestJinjaProjectRenderer:
     def test_should_render_clean_architecture_project_structure(self) -> None:
         config = ConfigSchemaMother.any()
-        renderer = JinjaProjectRenderer(env=JinjaEnvironment("test"))
+        renderer = JinjaProjectRenderer(env=JinjaEnvironment(str(test_resources_path())))
 
         project_structure = renderer.render(context_config=config)
 
