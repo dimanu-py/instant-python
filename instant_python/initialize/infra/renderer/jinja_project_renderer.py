6d93e9a61d2822068163cfe983be8c9ea189b3a4
dimanu-py <diegomtz126@gmail.com>
2025-10-31 23:43:35 +0100

    feat(initialize): implement rendering of project structure using YAML template

--- instant_python/initialize/infra/renderer/jinja_project_renderer.py
+++ instant_python/initialize/infra/renderer/jinja_project_renderer.py
@@ -1,11 +1,19 @@
+import yaml
+
 from instant_python.config.domain.config_schema import ConfigSchema
 from instant_python.initialize.domain.project_renderer import ProjectRenderer
 from instant_python.initialize.infra.renderer.jinja_environment import JinjaEnvironment
 
 
 class JinjaProjectRenderer(ProjectRenderer):
+    _MAIN_STRUCTURE_TEMPLATE_FILE = "main_structure.yml.j2"
+
     def __init__(self, env: JinjaEnvironment) -> None:
         self._env = env
 
     def render(self, context_config: ConfigSchema) -> list[dict]:
-        raise NotImplementedError
+        template_name = f"{context_config.template_type}/{self._MAIN_STRUCTURE_TEMPLATE_FILE}"
+        raw_project_structure = self._env.render_template(
+            name=template_name, context=context_config.to_primitives()
+        )
+        return yaml.safe_load(raw_project_structure)
