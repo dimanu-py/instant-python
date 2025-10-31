2f8ca1ddff01eea1e867ae2a6b3c48cdb419c8d4
dimanu-py <diegomtz126@gmail.com>
2025-10-31 23:51:14 +0100

    test(initialize): add test to verify project renderer includes file template content in project structure

--- test/initialize/infra/renderer/test_jinja_project_renderer.py
+++ test/initialize/infra/renderer/test_jinja_project_renderer.py
@@ -18,3 +18,25 @@ class TestJinjaProjectRenderer:
 
         expect(project_structure).to_not(be_none)
         expect(project_structure).to_not(be_empty)
+
+    def test_should_include_file_template_content_in_project_structure(self) -> None:
+        config = ConfigSchemaMother.with_template(
+            template=SupportedTemplates.STANDARD.value
+        )
+        renderer = JinjaProjectRenderer(env=JinjaEnvironment(str(test_resources_path())))
+
+        project_structure = renderer.render(context_config=config)
+
+        first_file = self._extract_first_file_from_structure(project_structure)
+        expect(first_file).to_not(be_none)
+        expect(first_file.get("content")).to_not(be_empty)
+
+    def _extract_first_file_from_structure(self, project_structure):
+        for item in project_structure:
+            if item.get("type") == "file":
+                return item
+            elif item.get("type") == "directory":
+                nested_file = self._extract_first_file_from_structure(item.get("children", []))
+                if nested_file:
+                    return nested_file
+        return None
