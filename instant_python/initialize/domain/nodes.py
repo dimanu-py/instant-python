2e88d75f678d5d422efed04f836723c751431363
dimanu-py <diegomtz126@gmail.com>
2025-11-03 18:18:04 +0100

    feat(initialize): add __repr__ method to File class

--- instant_python/initialize/domain/nodes.py
+++ instant_python/initialize/domain/nodes.py
@@ -4,5 +4,8 @@ class File:
         self._extension = extension
         self._content = content
 
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(name={self._name}, extension={self._extension})"
+
     def build_path_for(self, path: str) -> str:
         return f"{path}/{self._name}{self._extension}"
