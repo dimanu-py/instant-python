d720f7db78d24e273daa0808cf9b5d0af65e0889
dimanu-py <diegomtz126@gmail.com>
2025-11-03 21:08:12 +0100

    feat(writer): implement write method to create directories and __init__.py for Python modules

--- instant_python/initialize/infra/writer/file_system_project_writer.py
+++ instant_python/initialize/infra/writer/file_system_project_writer.py
@@ -1,8 +1,17 @@
+from pathlib import Path
+
 from instant_python.config.domain.config_schema import ConfigSchema
+from instant_python.initialize.domain.nodes import Directory
 from instant_python.initialize.domain.project_structure import ProjectStructure
 from instant_python.initialize.domain.project_writer import ProjectWriter
 
 
 class FileSystemProjectWriter(ProjectWriter):
-    def write(self, project_structure: ProjectStructure, config: ConfigSchema, destination) -> None:
-        raise NotImplementedError
+    def write(self, project_structure: ProjectStructure, config: ConfigSchema, destination: Path) -> None:
+        for node in project_structure:
+            write_path = node.build_path_for(destination)
+            if isinstance(node, Directory):
+                write_path.mkdir(parents=True, exist_ok=True)
+                if node._is_python_module:
+                    init_file_path = write_path / "__init__.py"
+                    init_file_path.touch(exist_ok=True)
