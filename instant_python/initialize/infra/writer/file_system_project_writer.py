7845c4d47188c0e13fbc5796042921afb252373e
dimanu-py <diegomtz126@gmail.com>
2025-11-03 21:24:34 +0100

    feat(initialize): add support for creating files in the file system

--- instant_python/initialize/infra/writer/file_system_project_writer.py
+++ instant_python/initialize/infra/writer/file_system_project_writer.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 from instant_python.config.domain.config_schema import ConfigSchema
-from instant_python.initialize.domain.nodes import Directory
+from instant_python.initialize.domain.nodes import Directory, File
 from instant_python.initialize.domain.project_structure import ProjectStructure
 from instant_python.initialize.domain.project_writer import ProjectWriter
 
@@ -15,3 +15,5 @@ class FileSystemProjectWriter(ProjectWriter):
                 if node._is_python_module:
                     init_file_path = write_path / "__init__.py"
                     init_file_path.touch(exist_ok=True)
+            elif isinstance(node, File):
+                write_path.touch(exist_ok=True)
