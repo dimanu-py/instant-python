bdfcd143205a16da9f2ae5d0a8a961b78f052180
dimanu-py <diegomtz126@gmail.com>
2025-11-03 18:22:08 +0100

    feat(initialize): add Directory class with path building and representation methods

--- instant_python/initialize/domain/nodes.py
+++ instant_python/initialize/domain/nodes.py
@@ -1,3 +1,6 @@
+from typing import TypeAlias, Union
+
+
 class File:
     def __init__(self, name: str, extension: str, content: str | None = None) -> None:
         self._name = name
@@ -9,3 +12,19 @@ class File:
 
     def build_path_for(self, path: str) -> str:
         return f"{path}/{self._name}{self._extension}"
+
+
+class Directory:
+    def __init__(self, name: str, is_python_module: bool, children: list["Node"]) -> None:
+        self._name = name
+        self._is_python_module = is_python_module
+        self._children = children
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(name={self._name}, is_python_module={self._is_python_module})"
+
+    def build_path_for(self, path: str) -> str:
+        return f"{path}/{self._name}"
+
+
+Node: TypeAlias = Union[File, Directory]
