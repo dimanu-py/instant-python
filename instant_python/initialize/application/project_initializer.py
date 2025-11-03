16a645c6560e4315de1f52413ce51eba12cd746c
dimanu-py <diegomtz126@gmail.com>
2025-11-03 20:07:47 +0100

    feat(initialize): add ProjectWriter dependency to ProjectInitializer

--- instant_python/initialize/application/project_initializer.py
+++ instant_python/initialize/application/project_initializer.py
@@ -1,13 +1,16 @@
 from instant_python.config.domain.config_schema import ConfigSchema
 from instant_python.initialize.domain.project_renderer import ProjectRenderer
+from instant_python.initialize.domain.project_writer import ProjectWriter
 
 
 class ProjectInitializer:
     def __init__(
         self,
         renderer: ProjectRenderer,
+        writer: ProjectWriter,
     ) -> None:
         self._project_renderer = renderer
+        self._writer = writer
 
     def execute(self, config: ConfigSchema) -> None:
         _ = self._project_renderer.render(context_config=config)
