0714922c35a2731dbaab8c56869a520f16eacffc
dimanu-py <diegomtz126@gmail.com>
2025-11-04 09:45:46 +0100

    test(initialize): refactor tests for directory creation and improve readability

--- test/initialize/domain/test_nodes.py
+++ test/initialize/domain/test_nodes.py
@@ -24,41 +24,49 @@ class TestFile:
 
         file.create(writer=self._file_writer, destination=self._SOME_PROJECT_PATH)
 
-        expect(self._file_writer.create_file).to(have_been_called_with(Path("my_project/sample.py"), self._EMPTY_CONTENT))
+        expect(self._file_writer.create_file).to(
+            have_been_called_with(Path("my_project/sample.py"), self._EMPTY_CONTENT)
+        )
 
     def test_should_create_file_with_content(self) -> None:
         file = File(name=self._SOME_NAME, extension=self._SOME_EXTENSION, content=self._SOME_CONTENT)
 
         file.create(writer=self._file_writer, destination=self._SOME_PROJECT_PATH)
 
-        expect(self._file_writer.create_file).to(have_been_called_with(Path("my_project/sample.py"), self._SOME_CONTENT))
+        expect(self._file_writer.create_file).to(
+            have_been_called_with(Path("my_project/sample.py"), self._SOME_CONTENT)
+        )
 
 
 class TestDirectory:
+    _PYTHON_MODULE_NAME = "awesome_module"
+    _STANDARD_DIRECTORY_NAME = "docs"
+    _SOME_PROJECT_PATH = Path("my_project")
+
+    def setup_method(self) -> None:
+        self._directory_writer = Spy(NodeWriter)
+
     def test_should_create_empty_directory(self) -> None:
-        directory = Directory(name="docs", is_python_module=False, children=[])
-        directory_writer = Spy(NodeWriter)
+        directory = Directory(name=self._STANDARD_DIRECTORY_NAME, is_python_module=False, children=[])
 
-        directory.create(writer=directory_writer, destination=Path("my_project"))
+        directory.create(writer=self._directory_writer, destination=self._SOME_PROJECT_PATH)
 
-        expect(directory_writer.create_directory).to(have_been_called_with(Path("my_project/docs")))
-        expect(directory_writer.create_file).to_not(have_been_called)
+        expect(self._directory_writer.create_directory).to(have_been_called_with(Path("my_project/docs")))
+        expect(self._directory_writer.create_file).to_not(have_been_called)
 
     def test_should_create_python_module(self) -> None:
-        directory = Directory(name="awesome_module", is_python_module=True, children=[])
-        directory_writer = Spy(NodeWriter)
+        directory = Directory(name=self._PYTHON_MODULE_NAME, is_python_module=True, children=[])
 
-        directory.create(writer=directory_writer, destination=Path("my_project"))
+        directory.create(writer=self._directory_writer, destination=Path("my_project"))
 
-        expect(directory_writer.create_directory).to(have_been_called_with(Path("my_project/awesome_module")))
-        expect(directory_writer.create_file).to(have_been_called_with(Path("my_project/awesome_module/__init__.py")))
+        expect(self._directory_writer.create_directory).to(have_been_called_with(Path("my_project/awesome_module")))
+        expect(self._directory_writer.create_file).to(have_been_called_with(Path("my_project/awesome_module/__init__.py")))
 
     def test_should_create_python_module_with_files_inside(self) -> None:
         files = [FileMother.empty() for _ in range(2)]
-        directory = Directory(name="awesome_module", is_python_module=True, children=files)
-        directory_writer = Spy(NodeWriter)
+        directory = Directory(name=self._PYTHON_MODULE_NAME, is_python_module=True, children=files)
 
-        directory.create(writer=directory_writer, destination=Path("my_project"))
+        directory.create(writer=self._directory_writer, destination=self._SOME_PROJECT_PATH)
 
-        expect(directory_writer.create_directory).to(have_been_called_with(Path("my_project/awesome_module")))
-        expect(directory_writer.create_file).to(have_been_called.exactly(3))
+        expect(self._directory_writer.create_directory).to(have_been_called_with(Path("my_project/awesome_module")))
+        expect(self._directory_writer.create_file).to(have_been_called.exactly(3))
