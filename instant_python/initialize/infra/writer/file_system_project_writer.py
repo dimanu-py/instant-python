ad2b78082da62cbb6b2fbed01194f86cea23604a
dimanu-py <diegomtz126@gmail.com>
2025-11-04 10:48:51 +0100

    refactor(initialize): simplify node writing logic by delegating to node's create method

--- instant_python/initialize/infra/writer/file_system_project_writer.py
+++ instant_python/initialize/infra/writer/file_system_project_writer.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
 from instant_python.config.domain.config_schema import ConfigSchema
-from instant_python.initialize.domain.nodes import Directory, File
 from instant_python.initialize.domain.project_structure import ProjectStructure
 from instant_python.initialize.domain.project_writer import ProjectWriter, NodeWriter
 
@@ -22,11 +21,4 @@ class FileSystemProjectWriter(ProjectWriter):
 
     def write(self, project_structure: ProjectStructure, config: ConfigSchema, destination: Path) -> None:
         for node in project_structure:
-            write_path = node._build_path_for(destination)
-            if isinstance(node, Directory):
-                write_path.mkdir(parents=True, exist_ok=True)
-                if node._is_python_module:
-                    init_file_path = write_path / "__init__.py"
-                    init_file_path.touch(exist_ok=True)
-            elif isinstance(node, File):
-                write_path.touch(exist_ok=True)
+            node.create(writer=self._node_writer, destination=destination)
