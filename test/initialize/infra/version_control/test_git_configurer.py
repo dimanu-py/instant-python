142aa2e960863b8967ee8c893936421293cdd8cd
dimanu-py <diegomtz126@gmail.com>
2025-11-05 16:19:23 +0100

    test(initialize): add tests for successful git repository configuration

--- test/initialize/infra/version_control/test_git_configurer.py
+++ test/initialize/infra/version_control/test_git_configurer.py
@@ -1,16 +1,39 @@
 import os
 
-from doublex import Mock, Mimic
+from doublex import Mock, Mimic, expect_call
+from doublex_expects import have_been_satisfied
+from expects import expect
 
 from instant_python.initialize.infra.env_manager.system_console import SystemConsole
+from instant_python.initialize.infra.version_control.git_configurer import GitConfigurer
 from test.config.domain.mothers.git_config_mother import GitConfigMother
+from test.initialize.infra.env_manager.mother.command_execution_result_mother import CommandExecutionResultMother
 from test.initialize.infra.version_control.mock_git_configurer import MockGitConfigurer
 
 
 class TestGitConfigurer:
+    _SUCCESSFUL_COMMAND_RESULT = CommandExecutionResultMother.success()
+    _FAILED_COMMAND_RESULT = CommandExecutionResultMother.failure()
+    _A_USERNAME = "test_user"
+    _A_EMAIL = "test_user@gmail.com"
+
     def setup_method(self) -> None:
         self._console = Mimic(Mock, SystemConsole)
         self._git_configurer = MockGitConfigurer(project_directory=os.getcwd())
+
+    def test_should_configure_git_repository_successfully(self) -> None:
+        configurer = GitConfigurer(project_directory=os.getcwd(), console=self._console)
+
+        self._should_create_repository()
+        self._should_set_user_information()
+        self._should_make_first_commit()
+
+        configurer.setup(GitConfigMother.with_parameters(
+            username=self._A_USERNAME,
+            email=self._A_EMAIL,
+        ))
+
+        expect(self._console).to(have_been_satisfied)
 
     def test_should_initialize_git_repository(self) -> None:
         self._git_configurer._initialize_repository()
@@ -48,3 +71,14 @@ class TestGitConfigurer:
             "git add .",
             'git commit -m "🎉 chore: initial commit"',
         )
+
+    def _should_create_repository(self) -> None:
+        expect_call(self._console).execute("git init").returns(self._SUCCESSFUL_COMMAND_RESULT)
+
+    def _should_set_user_information(self) -> None:
+        expect_call(self._console).execute(f"git config user.name {self._A_USERNAME}").returns(self._SUCCESSFUL_COMMAND_RESULT)
+        expect_call(self._console).execute(f"git config user.email {self._A_EMAIL}").returns(self._SUCCESSFUL_COMMAND_RESULT)
+
+    def _should_make_first_commit(self) -> None:
+        expect_call(self._console).execute("git add .").returns(self._SUCCESSFUL_COMMAND_RESULT)
+        expect_call(self._console).execute('git commit -m "🎉 chore: initial commit"').returns(self._SUCCESSFUL_COMMAND_RESULT)
