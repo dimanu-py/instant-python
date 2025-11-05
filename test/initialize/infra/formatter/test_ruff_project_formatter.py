8bbfb6167764053ef94b655cd007e96f6029ac8f
dimanu-py <diegomtz126@gmail.com>
2025-11-05 10:11:44 +0100

    test(initialize): add UvxProjectFormatter to test setup for improved project formatting validation

--- test/initialize/infra/formatter/test_ruff_project_formatter.py
+++ test/initialize/infra/formatter/test_ruff_project_formatter.py
@@ -1,11 +1,13 @@
 import os
 
 from test.initialize.infra.formatter.mock_project_formatter import MockRuffProjectFormatter
+from instant_python.initialize.infra.formatter.ruff_project_formatter import RuffProjectFormatter
 
 
 class TestRuffProjectFormatter:
     def setup_method(self) -> None:
         self._formatter = MockRuffProjectFormatter(project_directory=os.getcwd())
+        self._ruff_formatter = RuffProjectFormatter(project_directory=os.getcwd())
 
     def test_should_format_project_files(self) -> None:
         self._formatter.format()
