0fc5759fa40d8f3749d7adb0026f05ba5d230f43
dimanu-py <diegomtz126@gmail.com>
2025-11-05 10:18:35 +0100

    test(initialize): add test for RuffProjectFormatter execution with SystemConsole

--- test/initialize/infra/formatter/test_ruff_project_formatter.py
+++ test/initialize/infra/formatter/test_ruff_project_formatter.py
@@ -1,13 +1,27 @@
 import os
 
+from doublex import Mock, Mimic, expect_call
+from doublex_expects import have_been_satisfied
+from expects import expect
+
+from instant_python.initialize.infra.env_manager.system_console import SystemConsole
+from test.initialize.infra.env_manager.mother.command_execution_result_mother import CommandExecutionResultMother
 from test.initialize.infra.formatter.mock_project_formatter import MockRuffProjectFormatter
 from instant_python.initialize.infra.formatter.ruff_project_formatter import RuffProjectFormatter
 
 
 class TestRuffProjectFormatter:
     def setup_method(self) -> None:
+        self._console = Mimic(Mock, SystemConsole)
         self._formatter = MockRuffProjectFormatter(project_directory=os.getcwd())
-        self._ruff_formatter = RuffProjectFormatter(project_directory=os.getcwd())
+        self._ruff_formatter = RuffProjectFormatter(project_directory=os.getcwd(), console=self._console)
+
+    def test_should_execute_ruff_formatter(self) -> None:
+        expect_call(self._console).execute("uvx ruff format").returns(CommandExecutionResultMother.success())
+
+        self._ruff_formatter.format()
+
+        expect(self._console).to(have_been_satisfied)
 
     def test_should_format_project_files(self) -> None:
         self._formatter.format()
