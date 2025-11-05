12feee47d3aebefa88160dc761ae42fee4d3314c
dimanu-py <diegomtz126@gmail.com>
2025-11-05 08:03:03 +0100

    test(initialize): modify test to verify only that uv is not installed and do not install dependencies

--- test/initialize/infra/env_manager/test_uv_env_manager.py
+++ test/initialize/infra/env_manager/test_uv_env_manager.py
@@ -8,7 +8,6 @@ from instant_python.config.domain.dependency_config import DependencyConfig
 from instant_python.initialize.domain.env_manager import CommandExecutionError
 from instant_python.initialize.infra.env_manager.system_console import SystemConsole
 from instant_python.initialize.infra.env_manager.uv_env_manager import UvEnvManager
-from test.config.domain.mothers.dependency_config_mother import DependencyConfigMother
 from test.initialize.infra.env_manager.mock_uv_env_manager import MockUvEnvManager, MockUvEnvManagerWithError
 from test.initialize.infra.env_manager.mother.command_execution_result_mother import CommandExecutionResultMother
 
@@ -23,18 +22,16 @@ class TestUvEnvManager:
         self._console = Mimic(Mock, SystemConsole)
         self._uv_env_manager = UvEnvManager(project_directory=os.getcwd(), console=self._console)
 
-    def test_should_setup_environment_without_installing_uv_if_is_already_installed(self) -> None:
+    def test_should_setup_environment_without_installing_uv_when_is_already_installed(self) -> None:
         python_version = "3.12"
-        dependency = DependencyConfigMother.with_parameter(name="requests", version="latest")
 
         expect_call(self._console).execute(f"{self._UV_EXECUTABLE} --version").returns(self._SUCCESSFUL_COMMAND_RESULT)
         expect_call(self._console).execute(f"{self._UV_EXECUTABLE} python install {python_version}").returns(
             self._SUCCESSFUL_COMMAND_RESULT
         )
         expect_call(self._console).execute(f"{self._UV_EXECUTABLE} sync").returns(self._SUCCESSFUL_COMMAND_RESULT)
-        expect_call(self._console).execute(f"{self._UV_EXECUTABLE} add requests").returns(self._SUCCESSFUL_COMMAND_RESULT)
 
-        self._uv_env_manager.setup(python_version=python_version, dependencies=[dependency])
+        self._uv_env_manager.setup(python_version=python_version, dependencies=[])
 
         expect(self._console).to(have_been_satisfied)
 
