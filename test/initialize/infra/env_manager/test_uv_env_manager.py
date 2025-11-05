d1735bd94fea7b6c429c5489d68fdb043334a9dc
dimanu-py <diegomtz126@gmail.com>
2025-11-05 07:50:48 +0100

    test(initialize): add tests for UvEnvManager environment setup and installation

--- test/initialize/infra/env_manager/test_uv_env_manager.py
+++ test/initialize/infra/env_manager/test_uv_env_manager.py
@@ -1,20 +1,41 @@
 import os
 
-from doublex import Mock, Mimic
+from doublex import Mock, Mimic, expect_call
+from doublex_expects import have_been_satisfied
 from expects import expect, raise_error
 
 from instant_python.config.domain.dependency_config import DependencyConfig
 from instant_python.initialize.domain.env_manager import CommandExecutionError
 from instant_python.initialize.infra.env_manager.system_console import SystemConsole
 from instant_python.initialize.infra.env_manager.uv_env_manager import UvEnvManager
+from test.config.domain.mothers.dependency_config_mother import DependencyConfigMother
 from test.initialize.infra.env_manager.mock_uv_env_manager import MockUvEnvManager, MockUvEnvManagerWithError
+from test.initialize.infra.env_manager.mother.command_execution_result_mother import CommandExecutionResultMother
 
 
 class TestUvEnvManager:
+    _UV_EXECUTABLE = "~/.local/bin/uv"
+    _SUCCESSFUL_COMMAND_RESULT = CommandExecutionResultMother.success()
+    _FAILED_COMMAND_RESULT = CommandExecutionResultMother.failure()
+
     def setup_method(self) -> None:
         self._uv_dependency_manager = MockUvEnvManager(project_directory=os.getcwd())
         self._console = Mimic(Mock, SystemConsole)
         self._uv_env_manager = UvEnvManager(project_directory=os.getcwd(), console=self._console)
+
+    def test_should_setup_environment_without_installing_uv_if_is_already_installed(self) -> None:
+        python_version = "3.12"
+        dependency = DependencyConfigMother.with_parameter(name="requests", version="latest")
+
+        expect_call(self._console).execute(f"{self._UV_EXECUTABLE} --version").returns(self._SUCCESSFUL_COMMAND_RESULT)
+        expect_call(self._console).execute(f"{self._UV_EXECUTABLE} python install {python_version}").returns(
+            self._SUCCESSFUL_COMMAND_RESULT
+        )
+        expect_call(self._console).execute(f"{self._UV_EXECUTABLE} add requests").returns(self._SUCCESSFUL_COMMAND_RESULT)
+
+        self._uv_env_manager.setup(python_version=python_version, dependencies=[dependency])
+
+        expect(self._console).to(have_been_satisfied)
 
     def test_should_install_uv(self) -> None:
         self._uv_dependency_manager._install()
