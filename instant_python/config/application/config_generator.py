4ac7e158bb812c413927e5844f10d2ce19385fcd
dimanu-py <diegomtz126@gmail.com>
2025-11-06 09:16:38 +0100

    refactor(config): modify config generator to instantiate a ConfigSchema instead of using parser

--- instant_python/config/application/config_generator.py
+++ instant_python/config/application/config_generator.py
@@ -5,21 +5,17 @@ from instant_python.config.domain.question_wizard import QuestionWizard
 
 
 class ConfigGenerator:
-    def __init__(self, question_wizard: QuestionWizard, writer: ConfigWriter, parser: ConfigParser) -> None:
+    def __init__(self, question_wizard: QuestionWizard, writer: ConfigWriter) -> None:
         self._question_wizard = question_wizard
         self._writer = writer
-        self._parser = parser
 
     def execute(self) -> None:
         answers = self._ask_project_configuration_to_user()
-        config = self._validate_project_configuration(answers)
+        config = ConfigSchema.from_primitives(answers)
         self._save_configuration(config)
 
     def _save_configuration(self, config: ConfigSchema) -> None:
         self._writer.write(config)
-
-    def _validate_project_configuration(self, answers: dict) -> ConfigSchema:
-        return self._parser.parse(answers)
 
     def _ask_project_configuration_to_user(self) -> dict:
         return self._question_wizard.run()
--- test/config/application/test_config_generator.py
+++ test/config/application/test_config_generator.py
@@ -13,20 +13,13 @@ class TestConfigGenerator:
     def test_should_generate_config(self) -> None:
         question_wizard = Mock(QuestionWizard)
         config_writer = Mock(ConfigWriter)
-        config_parser = Mock(ConfigParser)
-        config_generator = ConfigGenerator(
-            question_wizard=question_wizard,
-            writer=config_writer,
-            parser=config_parser,
-        )
+        config_generator = ConfigGenerator(question_wizard=question_wizard, writer=config_writer)
         config = ConfigSchemaMother.any()
 
         expect_call(question_wizard).run().returns(config.to_primitives())
-        expect_call(config_parser).parse(config.to_primitives()).returns(config)
         expect_call(config_writer).write(config)
 
         config_generator.execute()
 
         expect(question_wizard).to(have_been_satisfied)
-        expect(config_parser).to(have_been_satisfied)
         expect(config_writer).to(have_been_satisfied)
