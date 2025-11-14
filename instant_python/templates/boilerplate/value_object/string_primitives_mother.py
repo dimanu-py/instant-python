1164d85d5a0e5410f492b6e6f327946cde20345b
dimanu-py <diegomtz126@gmail.com>
2025-11-14 10:31:26 +0100

    feat(templates): remove value object implementation templates now that this feature is provided by sindripy

--- instant_python/templates/boilerplate/value_object/string_primitives_mother.py
+++
@@ -1,7 +0,0 @@
-from test{{ "shared.domain.random_generator" | resolve_import_path(template.name) }} import RandomGenerator
-
-
-class StringPrimitivesMother:
-	@staticmethod
-	def any() -> str:
-		return RandomGenerator.word()
--- instant_python/templates/boilerplate/value_object/validation.py
+++
@@ -1,7 +0,0 @@
-from typing import Callable, Any
-
-
-def validate(func: Callable[..., Any]) -> Callable[..., Any]:
-	"""Mark a method as a validator for ValueObject validation."""
-	setattr(func, "_is_validator", True)
-	return func
