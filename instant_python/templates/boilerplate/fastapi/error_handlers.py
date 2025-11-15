8ac47733f7e33932a23be3bb5665275209d2ab50
dimanu-py <diegomtz126@gmail.com>
2025-11-15 14:20:40 +0100

    feat(templates): add sindri value objects error handler for fastapi application if value objects feature is selected

--- instant_python/templates/boilerplate/fastapi/error_handlers.py
+++ instant_python/templates/boilerplate/fastapi/error_handlers.py
@@ -7,7 +7,9 @@ from {{ general.source_name }}{{ "shared.infra.logger.file_logger" | resolve_imp
 {% endif %}
 from {{ general.source_name }}{{ "shared.infra.http.error_response" | resolve_import_path(template.name) }} import InternalServerError, UnprocessableEntityError
 from {{ general.source_name }}{{ "shared.domain.errors.domain_error" | resolve_import_path(template.name) }} import DomainError
-
+{% if "value_objects" in template.built_in_features %}
+from sindripy.value_objects import SindriValidationError
+{% endif %}
 
 {% if "logger" in template.built_in_features %}
 logger = create_file_logger(name="{{ general.slug }}")
@@ -51,6 +53,22 @@ async def validation_error_handler(
 		},
 	)
 	return await request_validation_exception_handler(request, exc)
+
+{% if "value_objects" in template.built_in_features %}
+async def sindri_validation_error_handler(
+        request: Request,
+        exc: SindriValidationError,
+) -> JSONResponse:
+    logger.error(
+        message=f"error - {request.url.path}",
+        details={
+            "error": {"message": str(exc)},
+            "method": request.method,
+            "source": request.url.path,
+        },
+    )
+    return UnprocessableEntityError().as_json()
+{% endif %}
 {% else %}
 async def unexpected_exception_handler(_: Request, __: Exception) -> JSONResponse:
 	return InternalServerError().as_json()
@@ -58,4 +76,13 @@ async def unexpected_exception_handler(_: Request, __: Exception) -> JSONRespons
 
 async def domain_error_handler(_: Request, __: DomainError) -> JSONResponse:
 	return UnprocessableEntityError().as_json()
+
+
+{% if "value_objects" in template.built_in_features %}
+async def sindri_validation_error_handler(
+        _: Request,
+        __: SindriValidationError,
+) -> JSONResponse:
+    return UnprocessableEntityError().as_json()
+{% endif %}
 {% endif %}
