3bcd3c9d858e342d9deca9bdc7bcd5db766de94b
dimanu-py <diegomtz126@gmail.com>
2025-12-01 21:11:09 +0100

    refactor(config): remove legacy config writer now that its behavior has been unified in the config repository

--- instant_python/config/domain/config_writer.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-from instant_python.shared.domain.config_schema import ConfigSchema
-
-
-class ConfigWriter(ABC):
-    @abstractmethod
-    def write(self, config: ConfigSchema) -> None:
-        raise NotImplementedError
