ce17cdc34177dc49cfc84c451f56904e54d3f141
dimanu-py <diegomtz126@gmail.com>
2025-12-01 16:20:37 +0100

    test(initialize): clean up yaml config repository test with a setup method to have repo instantiation centralized

--- test/initialize/infra/persistence/test_yaml_config_repository.py
+++ test/initialize/infra/persistence/test_yaml_config_repository.py
@@ -18,30 +18,30 @@ class TestYamlConfigRepository:
     _CONFIG_FILE = "base_ipy_config.yml"
     _A_PROJECT_NAME = "python-project"
 
+    def setup_method(self) -> None:
+        self._repository = YamlConfigRepository()
+
     def test_should_write_valid_config_on_working_directory(self) -> None:
         config = ConfigSchemaMother.any()
-        repository = YamlConfigRepository()
 
-        repository.write(config)
+        self._repository.write(config)
 
         expected_output_path = Path.cwd() / config.config_file_path
         expect(expected_output_path.exists()).to(be_true)
         expected_output_path.unlink()
 
     def test_should_read_existing_config_file(self) -> None:
-        repository = YamlConfigRepository()
         config_path = resources_path() / self._CONFIG_FILE
 
-        config = repository.read(config_path)
+        config = self._repository.read(config_path)
 
         expect(config).to_not(be_none)
         verify(json.dumps(config.to_primitives(), indent=2))
 
     def test_should_raise_error_when_file_to_read_does_not_exist(self) -> None:
-        repository = YamlConfigRepository()
         config_path = Path("non/existing/path/config.yml")
 
-        expect(lambda: repository.read(config_path)).to(raise_error(ConfigurationFileNotFound))
+        expect(lambda: self._repository.read(config_path)).to(raise_error(ConfigurationFileNotFound))
 
     def test_should_move_config_file_to_destination_folder(self) -> None:
         with tempfile.TemporaryDirectory() as temp_dir:
@@ -49,10 +49,8 @@ class TestYamlConfigRepository:
             config_file_temp = self._create_config_file_in_temp_dir(temp_dir_path)
             project_folder = self._create_project_folder_in_temp_dir(temp_dir_path)
 
-            repository = YamlConfigRepository()
-
-            config = repository.read(config_file_temp)
-            repository.move(config, temp_dir_path)
+            config = self._repository.read(config_file_temp)
+            self._repository.move(config, temp_dir_path)
 
             final_config_path = project_folder / "ipy.yml"
             expect(final_config_path.exists()).to(be_true)
