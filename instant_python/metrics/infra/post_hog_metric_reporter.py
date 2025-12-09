59aa3938c5c9c29af31b5f52f21245c5d0a058c5
dimanu-py <diegomtz126@gmail.com>
2025-12-09 11:16:17 +0100

    feat(metrics): create posthog client inside reporter to be able to send metrics

--- instant_python/metrics/infra/post_hog_metric_reporter.py
+++ instant_python/metrics/infra/post_hog_metric_reporter.py
@@ -1,9 +1,16 @@
+from posthog import Posthog
+
 from instant_python.metrics.domain.metric_reporter import MetricReporter
 from instant_python.metrics.infra.post_hog_config import PostHogConfig
 
 
 class PostHogMetricReporter(MetricReporter):
-    def __init__(self, config: PostHogConfig) -> None: ...
+    def __init__(self, config: PostHogConfig) -> None:
+        self._client = Posthog(
+            config.api_key,
+            host=config.host,
+            disabled=config.disabled_for_testing,
+        )
 
     def send(self, metrics: dict) -> None:
         raise NotImplementedError
