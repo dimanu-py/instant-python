adf99b7c7e16c3514606efbed1f99308c1ef2c90
dimanu-py <diegomtz126@gmail.com>
2025-12-10 10:45:11 +0100

    test(metrics): add unit test to verify same distinct ID is returned on consecutive calls

--- test/metrics/infra/test_user_identity_manager.py
+++ test/metrics/infra/test_user_identity_manager.py
@@ -32,3 +32,13 @@ class TestUserIdentityManager:
             expect(stored_distinct_id).to(have_keys("distinct_id"))
             expect(stored_distinct_id["distinct_id"]).to(equal(distinct_id))
 
+    def test_should_return_same_id_on_consecutive_calls(self) -> None:
+        with tempfile.TemporaryDirectory() as temp_dir:
+            config_dir = Path(temp_dir)
+            user_identity_manager = UserIdentityManager(config_dir=config_dir)
+
+            first_id = user_identity_manager.get_distinct_id()
+            second_id = user_identity_manager.get_distinct_id()
+
+            expect(first_id).to(equal(second_id))
+
