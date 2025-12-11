910c8d1502fa076167092ecad57dfd93314a9ccf
dimanu-py <diegomtz126@gmail.com>
2025-12-11 21:45:38 +0100

    feat(metrics): implement 'is_unknown' method in config snapshot class to determine if it has been properly read or not

--- instant_python/metrics/domain/config_snapshot.py
+++ instant_python/metrics/domain/config_snapshot.py
@@ -1,6 +1,16 @@
 class ConfigSnapshot:
-    def __init__(self, python_version: str, dependency_manager: str, template: str, built_in_features: list[str]) -> None:
+    _UNKNOWN = "unknown"
+
+    def __init__(
+        self, python_version: str, dependency_manager: str, template: str, built_in_features: list[str]
+    ) -> None:
         self._python_version = python_version
         self._dependency_manager = dependency_manager
         self._template = template
         self._built_in_features = built_in_features
+
+    def is_unknown(self) -> bool:
+        return all(
+            value == self._UNKNOWN or value == []
+            for value in [self._python_version, self._dependency_manager, self._template, self._built_in_features]
+        )
