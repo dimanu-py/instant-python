2ce0140eda8234fdcf722b7d0ef3c48ee612ce59
dimanu-py <diegomtz126@gmail.com>
2025-12-11 21:54:02 +0100

    refactor(metrics): update ConfigSnapshotCreator to utilize for_metrics method for improved config handling

--- instant_python/metrics/application/config_snapshot_creator.py
+++ instant_python/metrics/application/config_snapshot_creator.py
@@ -3,6 +3,7 @@ from typing import Any
 
 from instant_python.metrics.domain.config_snapshot import ConfigSnapshot
 from instant_python.shared.domain.config_repository import ConfigRepository
+from instant_python.shared.domain.config_schema import ConfigSchema
 
 
 class ConfigSnapshotCreator:
@@ -11,8 +12,7 @@ class ConfigSnapshotCreator:
 
     def execute(self, config_path: Path) -> Any:
         config = self._read_config_and_filter_metrics_values(config_path)
-        return ConfigSnapshot(**config)
+        return ConfigSnapshot(**config.for_metrics())
 
-    def _read_config_and_filter_metrics_values(self, config_path: Path) -> dict[str, str | list[str]]:
-        config = self._repository.read(config_path)
-        return config.for_metrics()
+    def _read_config_and_filter_metrics_values(self, config_path: Path) -> ConfigSchema:
+        return self._repository.read(config_path)
