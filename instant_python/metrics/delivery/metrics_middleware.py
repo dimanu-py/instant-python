a7f498437b503bf956ed9f40b95f1c6ebf9deaf2
dimanu-py <diegomtz126@gmail.com>
2025-12-11 21:57:43 +0100

    feat(metrics): add snapshot creator instance inside metrics middleware

--- instant_python/metrics/delivery/metrics_middleware.py
+++ instant_python/metrics/delivery/metrics_middleware.py
@@ -4,6 +4,7 @@ from typing import Any
 from click import Context
 from typer.core import TyperGroup
 
+from instant_python.metrics.application.config_snapshot_creator import ConfigSnapshotCreator
 from instant_python.metrics.application.usage_metrics_sender import UsageMetricsSender
 from instant_python.metrics.infra.post_hog_config import PostHogConfig
 from instant_python.metrics.infra.post_hog_metrics_reporter import PostHogMetricsReporter
@@ -14,6 +15,7 @@ from instant_python.shared.infra.persistence.yaml_config_repository import YamlC
 class MetricsMiddleware(TyperGroup):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
+        self._config_snapshot_creator = ConfigSnapshotCreator(repository=YamlConfigRepository())
         self._metrics_sender = UsageMetricsSender(
             repository=YamlConfigRepository(),
             reporter=PostHogMetricsReporter(
