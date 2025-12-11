2fbecb804c15b95228df6612abaadc5a74e50ef1
dimanu-py <diegomtz126@gmail.com>
2025-12-11 07:52:26 +0100

    feat(metrics): implement execute method in metrics sender use case to send usage metrics data

--- instant_python/metrics/application/usage_metrics_sender.py
+++ instant_python/metrics/application/usage_metrics_sender.py
@@ -1,4 +1,9 @@
+import platform
+from pathlib import Path
+
+from instant_python import __version__
 from instant_python.metrics.domain.metrics_reporter import MetricsReporter
+from instant_python.metrics.domain.usage_metrics_data import UsageMetricsData
 from instant_python.shared.domain.config_repository import ConfigRepository
 
 
@@ -8,4 +13,13 @@ class UsageMetricsSender:
         self._repository = repository
 
     def execute(self, command_name: str) -> None:
-        pass
+        config = self._repository.read(Path.cwd() / "ipy.yml")
+        metrics_data = UsageMetricsData(
+            ipy_version=__version__,
+            operating_system=platform.system(),
+            python_version=config.python_version,
+            command=command_name,
+            template=config.template_type,
+            built_in_features=config.template.built_in_features,
+        )
+        self._reporter.send(metrics_data)
