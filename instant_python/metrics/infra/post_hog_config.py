8cfd3c23ebe08ec54bb06d8ed7f646a7d8105a13
dimanu-py <diegomtz126@gmail.com>
2025-12-12 10:36:10 +0100

    feat(metrics): set default values for api_key and host in PostHogConfig

--- instant_python/metrics/infra/post_hog_config.py
+++ instant_python/metrics/infra/post_hog_config.py
@@ -5,8 +5,8 @@ from pydantic_settings import SettingsConfigDict, BaseSettings
 
 class PostHogConfig(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", extra="ignore", env_prefix="POST_HOG_")
-    api_key: str = ""
-    host: str = ""
+    api_key: str = Field(default="phc_mcDDYHvnYAon8FJ3sl33Jy22j1k3Ul5jTv8wgeG5TzP")
+    host: str = Field(default="https://eu.i.posthog.com")
     metrics_enabled: bool = Field(
         default=True,
         alias="IPY_METRICS_ENABLE",
