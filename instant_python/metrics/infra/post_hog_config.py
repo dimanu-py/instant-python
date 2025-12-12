841cd9f73a8d6aa779a05f48cc160fe5f33d0123
dimanu-py <diegomtz126@gmail.com>
2025-12-12 08:54:00 +0100

    feat(config): add metrics_enabled field to configuration for controlling metrics collection

--- instant_python/metrics/infra/post_hog_config.py
+++ instant_python/metrics/infra/post_hog_config.py
@@ -1,3 +1,5 @@
+from pydantic import Field
+
 from pydantic_settings import SettingsConfigDict, BaseSettings
 
 
@@ -5,3 +7,10 @@ class PostHogConfig(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", extra="ignore", env_prefix="POST_HOG_")
     api_key: str = ""
     host: str = ""
+    metrics_enabled: bool = Field(
+        default=True,
+        alias="IPY_METRICS_ENABLE",
+    )
+
+    def is_metrics_disabled(self) -> bool:
+        return not self.metrics_enabled
